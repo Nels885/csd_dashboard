63d95f3d41214a70f5bcaa545aeedadf5ca85e4e
Nels885 <voirin.lionel@gmail.com>
2019-06-19 12:33:54 +0200

    Modification of the calculation function of the number of products awaiting repair

--- api/views.py
+++ api/views.py
@@ -12,15 +12,15 @@ class CharData(APIView):
         rtx_nb = 0
         for prod in labels:
             if prod in ["DISPLAY", "SMEG"]:
-                prod_nb.append(Xelon.objects.filter(modele_produit__icontains=prod).count())
+                prod_nb.append(Xelon.objects.filter(modele_produit__icontains=prod, date_retour__isnull=False).count())
             elif prod == "RTx":
                 for rtx in ["RT3", "RT4", "RT5"]:
-                    rtx_nb += Xelon.objects.filter(modele_produit=rtx).count()
+                    rtx_nb += Xelon.objects.filter(modele_produit=rtx, date_retour__isnull=False).count()
             else:
-                prod_nb.append(Xelon.objects.filter(modele_produit=prod).count())
+                prod_nb.append(Xelon.objects.filter(modele_produit=prod, date_retour__isnull=False).count())
         prod_nb.append(rtx_nb)
         labels_nb = sum(prod_nb)
-        prod_nb.append(Xelon.objects.all().count() - labels_nb)
+        prod_nb.append(Xelon.objects.filter(date_retour__isnull=False).count() - labels_nb)
         labels.append("AUTRES")
         data = {
             "labels": labels,
