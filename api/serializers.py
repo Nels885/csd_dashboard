447b5e12576aa9c4dd7fe550fe81233bd59aba87
Nels885 <voirin.lionel@gmail.com>
2019-07-04 12:28:04 +0200

    Add API requests for the calibration software and added filtering parameters

--- api/serializers.py
+++ api/serializers.py
@@ -5,37 +5,70 @@ from squalaetp.models import Xelon, Corvet
 from raspeedi.models import Raspeedi
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    """
+    A ModelSerializer that takes an additional `fields` argument that
+    controls which fields should be displayed.
+    """
 
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop('fields', None)
+
+        # Instantiate the superclass normally
+        super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
+
+        if fields is not None:
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
-
     class Meta:
         model = Group
         fields = ('url', 'name')
 
 
 class RaspeediSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Raspeedi
-        fields = ('ref_boitier', 'produit', 'facade', 'type', 'dump_peedi', 'media')
+        fields = ('ref_boitier', 'produit', 'facade', 'dump_peedi', 'media', 'dump_renesas',)
 
 
-class CorvetSerializer(serializers.ModelSerializer):
-    raspeedi = RaspeediSerializer(many=True)
+class CorvetSerializer(DynamicFieldsModelSerializer):
+    ref_radio = serializers.CharField(source='electronique_14l')
+    cal_radio = serializers.CharField(source='electronique_94l')
+    ref_nav = serializers.CharField(source='electronique_14x')
+    cal_nav = serializers.CharField(source='electronique_94x')
+    no_serie = serializers.CharField(source='electronique_44x')
+    raspeedi = RaspeediSerializer(many=True, read_only=True)
 
     class Meta:
         model = Corvet
-        fields = ('electronique_14l', 'electronique_94l', 'electronique_14x', 'electronique_94x', 'raspeedi')
+        fields = ('ref_radio', 'cal_radio', 'ref_nav', 'cal_nav', 'no_serie', 'raspeedi')
 
 
 class ProgSerializer(serializers.ModelSerializer):
     corvet = CorvetSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Xelon
+        fields = ('numero_de_dossier', 'vin', 'modele_produit', 'modele_vehicule', 'corvet')
+        depth = 1
+
+
+class CalSerializer(serializers.ModelSerializer):
+    corvet = CorvetSerializer(many=True, read_only=True,
+                              fields=('ref_radio', 'cal_radio', 'ref_nav', 'cal_nav', 'no_serie'))
 
     class Meta:
         model = Xelon
--- api/views.py
+++ api/views.py
@@ -4,7 +4,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import viewsets, permissions
 
-from api.serializers import UserSerializer, GroupSerializer, ProgSerializer
+
+from api.serializers import UserSerializer, GroupSerializer, ProgSerializer, CalSerializer, RaspeediSerializer
+from raspeedi.models import Raspeedi
 from squalaetp.models import Xelon
 from .utils import products_count
 
@@ -40,10 +42,46 @@ class ProgList(generics.ListAPIView):
         :return:
             Serialized data
         """
-        queryset = Xelon.objects.all().prefetch_related('corvet')
         customer_file = self.request.query_params.get('xelon', None)
-        if customer_file is not None:
+        vin = self.request.query_params.get('vin', None)
+        ref_case = self.request.query_params.get('ref', None)
+        if customer_file and vin:
+            queryset = Xelon.objects.filter(numero_de_dossier=customer_file, vin=vin).prefetch_related('corvet')
+        elif customer_file:
             queryset = Xelon.objects.filter(numero_de_dossier=customer_file).prefetch_related('corvet')
+        elif vin:
+            queryset = Xelon.objects.filter(vin=vin).prefetch_related('corvet')
+        elif ref_case:
+            self.serializer_class = RaspeediSerializer
+            queryset = Raspeedi.objects.filter(ref_boitier=ref_case)
+        else:
+            queryset = Xelon.objects.all().prefetch_related('corvet')
+        return queryset
+
+
+class CalList(generics.ListAPIView):
+    """
+    API endpoint that allows prog list to be viewed
+    """
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = CalSerializer
+
+    def get_queryset(self):
+        """
+        Provides the list of desired data
+        :return:
+            Serialized data
+        """
+        customer_file = self.request.query_params.get('xelon', None)
+        vin = self.request.query_params.get('vin', None)
+        if customer_file and vin:
+            queryset = Xelon.objects.filter(numero_de_dossier=customer_file, vin=vin).prefetch_related('corvet')
+        elif customer_file:
+            queryset = Xelon.objects.filter(numero_de_dossier=customer_file).prefetch_related('corvet')
+        elif vin:
+            queryset = Xelon.objects.filter(vin=vin).prefetch_related('corvet')
+        else:
+            queryset = Xelon.objects.all().prefetch_related('corvet')
         return queryset
 
 
