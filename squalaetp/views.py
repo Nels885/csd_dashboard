608ddd236d4add13c16acf2be49e7de94526ff62
Nels885 <voirin.lionel@gmail.com>
2019-09-03 17:38:45 +0200

    Fix problem with scraping and modifying readme.md file

--- squalaetp/views.py
+++ squalaetp/views.py
@@ -5,6 +5,7 @@ from django.utils.translation import ugettext as _
 from .models import Xelon, Corvet
 from .forms import CorvetForm
 from dashboard.forms import ParaErrorList
+from utils.scraping import ScrapingCorvet
 
 
 def xelon_table(request):
@@ -86,8 +87,11 @@ def xelon_edit(request, file_id):
                     form.add_error('internal', _('An internal error has occurred. Thank you recommend your request'))
         context['errors'] = form.errors.items()
     else:
+        corvet = ScrapingCorvet()
         form = CorvetForm()
         form.fields['vin'].initial = file.vin
+        form.fields['xml_data'].initial = corvet.result(file.vin)
+        corvet.close()
     context['form'] = form
     return render(request, 'squalaetp/xelon_edit.html', context)
 
--- utils/scraping.py
+++ utils/scraping.py
@@ -1,6 +1,10 @@
 import os
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class ScrapingCorvet(webdriver.Firefox):
@@ -12,8 +16,8 @@ class ScrapingCorvet(webdriver.Firefox):
         options = Options()
         options.add_argument('-headless')
         super().__init__(firefox_options=options)
-        self.get(self.START_URLS)
         self.implicitly_wait(10)
+        self.get(self.START_URLS)
 
     def result(self, vin_value=None):
         """
@@ -21,27 +25,26 @@ class ScrapingCorvet(webdriver.Firefox):
         :param vin_value: VIN number for the Corvet data
         :return: Corvet data
         """
-        data = None
-        for _ in range(2):
-            self.login()
-            vin = self.find_element_by_name('form:input_vin')
-            submit = self.find_element_by_id('form:suite')
-            if vin_value:
-                vin.send_keys(vin_value)
-            else:
-                vin.clear()
-            submit.click()
-            corvet = self.find_element_by_name('form:resultat_CORVET')
-            data = corvet.text
-            self.logout()
+        self.login()
+        vin = self.find_element_by_id('form:input_vin')
+        submit = self.find_element_by_id('form:suite')
+        vin.clear()
+        if vin_value:
+            vin.send_keys(vin_value)
+        submit.click()
+        time.sleep(3)
+        data = WebDriverWait(self, 10).until(
+            EC.presence_of_element_located((By.ID, 'form:resultat_CORVET'))
+        ).text
+        self.logout()
         return data
 
     def login(self):
         """
         Login on the web site
         """
-        username = self.find_element_by_name('form:identifiant2')
-        password = self.find_element_by_name('form:password2')
+        username = self.find_element_by_id('form:identifiant2')
+        password = self.find_element_by_id('form:password2')
         login = self.find_elements_by_id('form:login2')
         username.send_keys(os.environ.get('USER_CORVET'))
         password.send_keys(os.environ.get('PWD_CORVET'))
