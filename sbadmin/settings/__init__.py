d9ac5fd9e44d9b9e27b5eab019523b635c6457da
Nels885 <voirin.lionel@gmail.com>
2019-09-20 08:26:50 +0200

    Modifying the settings modules for Excel Clarion file constants

--- sbadmin/settings/__init__.py
+++ sbadmin/settings/__init__.py
@@ -154,3 +154,21 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
 LOGIN_REDIRECT_URL = '/dashboard/profile/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+# Config Clarion
+
+XLS_ROOT = os.path.abspath(os.path.expanduser('~') + '/Documents/CSD_DATABASE')
+
+XLS_RASPEEDI_FILE = os.path.join(XLS_ROOT, "PROG/RASPEEDI/table_boitier_PSA.xlsx")
+XLS_SQUALAETP_FILE = os.path.join(XLS_ROOT, "EXTS/squalaetp.xls")
+XLS_ATTRIBUTS_FILE = os.path.join(XLS_ROOT, "EXTS/Attributs CORVET.xlsx")
+XLS_DELAY_FILES = [
+    os.path.join(XLS_ROOT, "RH/AnalyseRetards/PSA.xls"),
+    os.path.join(XLS_ROOT, "RH/AnalyseRetards/ILOTAUTRE.xls"),
+    os.path.join(XLS_ROOT, "RH/AnalyseRetards/LaboQual.xls"),
+    os.path.join(XLS_ROOT, "RH/AnalyseRetards/DEFAUT.xls"),
+    os.path.join(XLS_ROOT, "RH/AnalyseRetards/CLARION.xls"),
+]
+
+XML_PATH = os.path.join(XLS_ROOT, 'LOGS/CORVET_XML_TEST')
