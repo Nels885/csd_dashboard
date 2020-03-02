174b88916b1e427a7b96c64586bc2932a8917756
Nels885 <voirin.lionel@gmail.com>
2020-03-02 12:04:44 +0100

    Update translation and reduction of padding for nav-bar

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import Permission
 
 from .models import UserProfile, Post
 
+admin.site.register(Permission)
 admin.site.register(UserProfile)
 admin.site.register(Post)
