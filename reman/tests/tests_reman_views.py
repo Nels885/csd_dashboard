63017e9d361a1b994784d0ffd6770b5c937c1bb8
Nels885 <voirin.lionel@gmail.com>
2020-03-26 09:57:56 +0100

    Added missing tests for the Reman app

--- reman/tests/tests_reman_views.py
+++ reman/tests/tests_reman_views.py
@@ -13,7 +13,7 @@ class RemanTestCase(UnitTest):
         self.add_perms_user(Repair, 'add_repair', 'view_repair', 'change_repair')
         self.add_perms_user(SparePart, 'add_sparepart', 'view_sparepart')
         ecu = EcuModel.objects.create(es_reference='1234567890', oe_reference='160000000',
-                                      oe_raw_reference='1699999999', technical_data='test')
+                                      oe_raw_reference='1699999999', hw_reference='9876543210', technical_data='test')
         batch = Batch.objects.create(number=1, quantity=10, created_by=self.user, ecu_model=ecu)
         self.repair = Repair.objects.create(batch=batch, created_by=self.user)
 
@@ -26,11 +26,11 @@ class RemanTestCase(UnitTest):
         response = self.client.get(reverse('reman:repair_table'))
         self.assertEqual(response.status_code, 200)
 
-    def test_part_table_page_is_disconnected(self):
+    def test_spare_part_table_page_is_disconnected(self):
         response = self.client.get(reverse('reman:part_table'))
         self.assertRedirects(response, '/accounts/login/?next=/reman/part/table/', status_code=302)
 
-    def test_part_table_page_is_connected(self):
+    def test_spare_part_table_page_is_connected(self):
         self.login()
         response = self.client.get(reverse('reman:part_table'))
         self.assertEqual(response.status_code, 200)
