072829351e55e76c712b1a72d21c6620673e3f40
Nels885 <voirin.lionel@gmail.com>
2020-04-16 09:47:51 +0200

    Addition of the column 'Modele produit' for CORVET data extraction

--- import_export/views.py
+++ import_export/views.py
@@ -57,16 +57,17 @@ def export_corvet_csv(request):
 def export_ecu_csv(request):
     filename = 'ecu'
     header = [
-        'Numero de dossier', 'V.I.N.', 'DATE_DEBUT_GARANTIE', '14A_CMM_HARD', '34A_CMM_SOFT_LIVRE', '94A_CMM_SOFT',
-        '44A_CMM_FOURN.NO.SERIE', '54A_CMM_FOURN.DATE.FAB', '64A_CMM_FOURN.CODE', '84A_CMM_DOTE', 'P4A_CMM_EOBD'
+        'Numero de dossier', 'V.I.N.', 'Modele produit', 'DATE_DEBUT_GARANTIE', '14A_CMM_HARD', '34A_CMM_SOFT_LIVRE',
+        '94A_CMM_SOFT', '44A_CMM_FOURN.NO.SERIE', '54A_CMM_FOURN.DATE.FAB', '64A_CMM_FOURN.CODE', '84A_CMM_DOTE',
+        'P4A_CMM_EOBD'
     ]
     ecus = Xelon.objects.filter(corvet__isnull=False).exclude(corvet__electronique_14a__exact='').annotate(
         date_debut_garantie=Cast(TruncSecond('corvet__donnee_date_debut_garantie', DateTimeField()), CharField())
     )
     values_list = (
-        'numero_de_dossier', 'vin', 'date_debut_garantie', 'corvet__electronique_14a', 'corvet__electronique_34a',
-        'corvet__electronique_94a', 'corvet__electronique_44a', 'corvet__electronique_54a', 'corvet__electronique_64a',
-        'corvet__electronique_84a', 'corvet__electronique_p4a'
+        'numero_de_dossier', 'vin', 'modele_produit', 'date_debut_garantie', 'corvet__electronique_14a',
+        'corvet__electronique_34a', 'corvet__electronique_94a', 'corvet__electronique_44a', 'corvet__electronique_54a',
+        'corvet__electronique_64a', 'corvet__electronique_84a', 'corvet__electronique_p4a'
     )
 
     return export_csv(queryset=ecus, filename=filename, header=header, values_list=values_list)
@@ -76,16 +77,17 @@ def export_ecu_csv(request):
 def export_bsi_csv(request):
     filename = 'bsi'
     header = [
-        'Numero de dossier', 'V.I.N.', 'DATE_DEBUT_GARANTIE', '14B_BSI_HARD', '94B_BSI_SOFT', '44B_BSI_FOURN.NO.SERIE',
-        '54B_BSI_FOURN.DATE.FAB', '64B_BSI_FOURN.CODE', '84B_BSI_DOTE'
+        'Numero de dossier', 'V.I.N.', 'Modele produit', 'DATE_DEBUT_GARANTIE', '14B_BSI_HARD', '94B_BSI_SOFT',
+        '44B_BSI_FOURN.NO.SERIE', '54B_BSI_FOURN.DATE.FAB', '64B_BSI_FOURN.CODE', '84B_BSI_DOTE'
     ]
 
     bsis = Xelon.objects.filter(corvet__isnull=False).exclude(corvet__electronique_14b__exact='').annotate(
         date_debut_garantie=Cast(TruncSecond('corvet__donnee_date_debut_garantie', DateTimeField()), CharField())
     )
     values_list = (
-        'numero_de_dossier', 'vin', 'date_debut_garantie', 'corvet__electronique_14b', 'corvet__electronique_94b',
-        'corvet__electronique_44b', 'corvet__electronique_54b', 'corvet__electronique_64b', 'corvet__electronique_84b',
+        'numero_de_dossier', 'vin', 'modele_produit', 'date_debut_garantie', 'corvet__electronique_14b',
+        'corvet__electronique_94b', 'corvet__electronique_44b', 'corvet__electronique_54b', 'corvet__electronique_64b',
+        'corvet__electronique_84b',
     )
 
     return export_csv(queryset=bsis, filename=filename, header=header, values_list=values_list)
@@ -95,16 +97,16 @@ def export_bsi_csv(request):
 def export_com_csv(request):
     filename = 'com200x'
     header = [
-        'Numero de dossier', 'V.I.N.', 'DATE_DEBUT_GARANTIE', '16P_HDC_HARD', '46P_HDC_FOURN.NO.SERIE',
-        '56P_HDC_FOURN.DATE.FAB', '66P_HDC_FOURN.CODE'
+        'Numero de dossier', 'V.I.N.', 'Modele produit', 'DATE_DEBUT_GARANTIE', '16P_HDC_HARD',
+        '46P_HDC_FOURN.NO.SERIE', '56P_HDC_FOURN.DATE.FAB', '66P_HDC_FOURN.CODE'
     ]
 
     bsis = Xelon.objects.filter(corvet__isnull=False).exclude(corvet__electronique_16p__exact='').annotate(
         date_debut_garantie=Cast(TruncSecond('corvet__donnee_date_debut_garantie', DateTimeField()), CharField())
     )
     values_list = (
-        'numero_de_dossier', 'vin', 'date_debut_garantie', 'corvet__electronique_16p', 'corvet__electronique_46p',
-        'corvet__electronique_56p', 'corvet__electronique_66p'
+        'numero_de_dossier', 'vin', 'modele_produit', 'date_debut_garantie', 'corvet__electronique_16p',
+        'corvet__electronique_46p', 'corvet__electronique_56p', 'corvet__electronique_66p'
     )
 
     return export_csv(queryset=bsis, filename=filename, header=header, values_list=values_list)
