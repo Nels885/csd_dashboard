7384c0375aa8f870093714a6fdf45eb43018bd9c
Nels885 <voirin.lionel@gmail.com>
2020-04-27 10:22:16 +0200

    Reorganization of project templates

--- sbadmin/settings/__init__.py
+++ sbadmin/settings/__init__.py
@@ -72,7 +72,9 @@ ROOT_URLCONF = 'sbadmin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(os.path.dirname(BASE_DIR), 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
