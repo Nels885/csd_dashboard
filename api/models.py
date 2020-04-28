6d1526f678174d7375f9b8906277145634ae56a4
Nels885 <voirin.lionel@gmail.com>
2020-04-28 13:19:22 +0200

    Simplification of the code for filtering data from Corvet and Xelon tables with ServerSide from Datatables

--- api/models.py
+++ api/models.py
@@ -1,28 +1,25 @@
+import operator
+from functools import reduce
 from model_utils import Choices
 from django.db.models import Q
 
 from squalaetp.models import Xelon, Corvet
 
+XELON_COLUMN_LIST = [
+    'numero_de_dossier', 'vin', 'modele_produit', 'modele_vehicule', 'date_retour', 'type_de_cloture', 'nom_technicien'
+]
+
+CORVET_COLUMN_LIST = [
+    'vin', 'electronique_14f', 'electronique_94f', 'electronique_14x', 'electronique_14a', 'electronique_94a',
+    'electronique_14b', 'electronique_94b'
+]
+
 ORDER_XELON_COLUMN_CHOICES = Choices(
-    ('2', 'numero_de_dossier'),
-    ('3', 'vin'),
-    ('4', 'modele_produit'),
-    ('5', 'modele_vehicule'),
-    ('6', 'date_retour'),
-    ('7', 'type_de_cloture'),
-    ('8', 'nom_technicien'),
+    *[(str(i), elt, elt) for i, elt in enumerate(XELON_COLUMN_LIST, start=2)]
 )
 
 ORDER_CORVET_COLUMN_CHOICES = Choices(
-    ('1', 'vin'),
-    ('2', 'electronique_14f'),
-    ('3', 'electronique_94f'),
-    ('4', 'electronique_14x'),
-    ('5', 'electronique_94x'),
-    ('6', 'electronique_14a'),
-    ('7', 'electronique_94a'),
-    ('8', 'electronique_14b'),
-    ('9', 'electronique_94b'),
+    *[(str(i), elt, elt) for i, elt in enumerate(CORVET_COLUMN_LIST, start=1)]
 )
 
 
@@ -55,25 +52,12 @@ def query_table_by_args(filter, queryset, order_choices, **kwargs):
 
 
 def xelon_filter(queryset, search_value):
-    queryset = queryset.filter(Q(numero_de_dossier__icontains=search_value) |
-                               Q(vin__icontains=search_value) |
-                               Q(modele_produit__icontains=search_value) |
-                               Q(modele_vehicule__icontains=search_value) |
-                               Q(date_retour__icontains=search_value) |
-                               Q(type_de_cloture__icontains=search_value) |
-                               Q(nom_technicien__icontains=search_value))
+    object_q = [Q((col + "__icontains", search_value)) for col in XELON_COLUMN_LIST]
+    queryset = queryset.filter(reduce(operator.or_, object_q))
     return queryset
 
 
 def corvet_filter(queryset, search_value):
-    queryset = queryset.filter(Q(vin__icontains=search_value) |
-                               Q(electronique_14f__icontains=search_value) |
-                               Q(electronique_94f__icontains=search_value) |
-                               Q(electronique_94x__icontains=search_value) |
-                               Q(electronique_14x__icontains=search_value) |
-                               Q(electronique_14a__icontains=search_value) |
-                               Q(electronique_14a__icontains=search_value) |
-                               Q(electronique_94a__icontains=search_value) |
-                               Q(electronique_14b__icontains=search_value) |
-                               Q(electronique_94b__icontains=search_value))
+    object_q = [Q((col + "__icontains", search_value)) for col in CORVET_COLUMN_LIST]
+    queryset = queryset.filter(reduce(operator.or_, object_q))
     return queryset
