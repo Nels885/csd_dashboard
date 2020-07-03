c181b5b6b6d2581e8dba8d31123dcaaf992df258
Nels885 <voirin.lionel@gmail.com>
2020-07-03 13:57:40 +0200

    Adding search fields for REMAN admin templates

--- reman/admin.py
+++ reman/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
-from .models import Batch, EcuModel, Repair, SparePart, Default, EcuRefBase
+from .models import Batch, EcuModel, Repair, SparePart, Default, EcuRefBase, EcuType
 
 
 class BatchAdmin(admin.ModelAdmin):
     list_display = ('batch_number', 'quantity', 'created_by', 'created_at', 'active')
+    ordering = ('batch_number',)
+    search_fields = ('batch_number',)
 
     def batch_number(self, obj):
         return obj
@@ -14,6 +16,8 @@ class BatchAdmin(admin.ModelAdmin):
 
 class SparePartAdmin(admin.ModelAdmin):
     list_display = ('code_produit', 'code_zone', 'code_emplacement', 'cumul_dispo')
+    ordering = ('code_produit',)
+    search_fields = ('code_produit', 'code_zone', 'code_emplacement')
 
 
 class EcuModelAdmin(admin.ModelAdmin):
@@ -24,6 +28,7 @@ class EcuModelAdmin(admin.ModelAdmin):
 
 admin.site.register(Batch, BatchAdmin)
 admin.site.register(EcuRefBase)
+admin.site.register(EcuType)
 admin.site.register(EcuModel, EcuModelAdmin)
 admin.site.register(Repair)
 admin.site.register(SparePart, SparePartAdmin)
