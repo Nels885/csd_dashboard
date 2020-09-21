9d5bc4eae5ab0ede45de4aec81270c05bd4054d6
Nels885 <voirin.lionel@gmail.com>
2020-09-21 14:55:58 +0200

    Adding firmware version for NAC Wave3 in PSA tools

--- psa/forms.py
+++ psa/forms.py
@@ -4,7 +4,10 @@ from utils.django.validators import validate_nac
 
 
 class NacLicenseForm(forms.Form):
-    SOFTWARES = [('001315031548167166', '7-3-1-r1_WAVE1'), ('001315031579159852', '21-08-25-12_NAC-r0_WAVE2')]
+    SOFTWARES = [
+        ('001315031548167166', '7-3-1-r1_WAVE1'), ('001315031579159852', '21-08-25-12_NAC-r0_WAVE2'),
+        ('001315031542612351', '31-09-46-62_NAC-r0_WAVE3'),
+    ]
 
     software = forms.ChoiceField(
         label='Version Software', required=True, choices=SOFTWARES,
@@ -25,6 +28,7 @@ class NacLicenseForm(forms.Form):
 
 class NacUpdateForm(forms.Form):
     SOFTWARES = [('001315031548167166', '7-3-1-r1_WAVE1'), ('001315031579159852', '21-08-25-12_NAC-r0_WAVE2'),
+                 ('001315031542612351', '31-09-46-62_NAC-r0_WAVE3'),
                  ('001315031576311163', '21-08-24-12_NAC-r1_WAVE2'), ('001315031563976162', '21-08-22-32_NAC-r1_WAVE2'),
                  ('001315031560945905', '21-07-67-32_NAC-r0_WAVE2'), ('001315031551279802', '21-07-67-32_NAC-r0_WAVE2'),
                  ('001315031524207679', '21-07-16-32_NAC-r0_WAVE2'), ('001315031511946541', '21-06-47-34_NAC-r0_WAVE2'),
