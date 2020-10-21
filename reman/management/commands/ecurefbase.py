a08c85d97819d009c65dc081f1ed7e00ea1e24ce
Nels885 <voirin.lionel@gmail.com>
2020-10-21 15:24:35 +0200

    Modification and improvement of the ecurefbase cmd of the REMAN app

--- reman/management/commands/ecurefbase.py
+++ reman/management/commands/ecurefbase.py
@@ -7,6 +7,7 @@ from squalaetp.models import ProductCode, Stock
 from reman.models import EcuRefBase, EcuModel, SparePart, EcuType
 from utils.conf import XLS_ECU_REF_BASE, CSD_ROOT, conf
 from utils.file.export import ExportExcel, os
+from utils.django.models import defaults_dict
 
 from ._excel_reman import ExcelEcuRefBase
 
@@ -58,116 +59,114 @@ class Command(BaseCommand):
                     cursor.execute(sql)
             self.stdout.write(self.style.WARNING("Suppression des données de la table EcuRefBase terminée!"))
         elif options['export']:
-            filename = 'base_ref_reman_new'
-            path = os.path.join(CSD_ROOT, conf.EXPORT_PATH)
-            header = [
-                'Reference OE', 'REFERENCE REMAN', 'Module Moteur', 'Réf HW', 'FNR', 'CODE BARRE PSA', 'REF FNR',
-                'REF CAL OUT', 'REF à créer ', 'REF_PSA_OUT', 'OPENDIAG', 'REF_MAT', 'REF_COMP', 'CAL_KTAG', 'STATUT'
-            ]
-            queryset = EcuModel.objects.all().order_by('ecu_type__ecu_ref_base__reman_reference')
-            values_list = (
-                'oe_raw_reference', 'ecu_type__ecu_ref_base__reman_reference', 'ecu_type__technical_data',
-                'ecu_type__hw_reference', 'ecu_type__supplier_oe', 'psa_barcode', 'former_oe_reference',
-                'ecu_type__ecu_ref_base__ref_cal_out', 'ecu_type__spare_part__code_produit',
-                'ecu_type__ecu_ref_base__ref_psa_out', 'ecu_type__ecu_ref_base__open_diag',
-                'ecu_type__ecu_ref_base__ref_mat', 'ecu_type__ecu_ref_base__ref_comp',
-                'ecu_type__ecu_ref_base__cal_ktag',
-                'ecu_type__ecu_ref_base__status'
-
-            )
-            ExportExcel(queryset=queryset, filename=filename, header=header, values_list=values_list).file(path, False)
-            self.stdout.write(
-                self.style.SUCCESS(
-                    "Export ECU_REF_BASE completed: NB_REF = {} | FILE = {}.csv".format(queryset.count(), filename))
-            )
+            self._export()
         else:
             if options['filename'] is not None:
                 extraction = ExcelEcuRefBase(options['filename'], sheet_name=options['sheet_id'])
             else:
                 extraction = ExcelEcuRefBase(XLS_ECU_REF_BASE, sheet_name=options['sheet_id'])
+            self._update_or_create(extraction.read_all())
 
-            nb_base_before, nb_ecu_before = EcuRefBase.objects.count(), EcuModel.objects.count()
-            nb_base_update, nb_ecu_update, nb_part_update, nb_type_update = 0, 0, 0, 0
-            for row in extraction.read_all():
-                log.info(row)
-                reman_reference = row.pop("reman_reference")
-                type_values = dict(
-                    (key, value) for key, value in row.items() if key in [
-                        "technical_data", "supplier_oe"
-                    ] and value != ""
+    def _update_or_create(self, data):
+
+        nb_base_before, nb_ecu_before = EcuRefBase.objects.count(), EcuModel.objects.count()
+        nb_base_update, nb_ecu_update, nb_part_update, nb_type_update = 0, 0, 0, 0
+        for row in data:
+            log.info(row)
+            code_produit = reman_reference = None
+            try:
+                code_produit, reman_reference = row["code_produit"], row["reman_reference"]
+
+                # Update or Create SpareParts
+                part_obj, part_created = SparePart.objects.update_or_create(
+                    code_produit=code_produit, defaults={
+                        "code_zone": "REMAN PSA", "code_magasin": "MAGREM PSA"
+                    }
                 )
-                for key in ["technical_data", "supplier_oe"]:
-                    del row[key]
-                code_produit = row.pop("code_produit")
-                base_values = dict(
-                    (key, value) for key, value in row.items() if key in [
-                        "ref_cal_out", "ref_psa_out", "open_diag", "ref_mat", "ref_comp", "cal_ktag", "status"
-                    ] and value != ""
+                if not part_created:
+                    nb_part_update += 1
+
+                # Update or create StockParts
+                prod_obj, part_created = ProductCode.objects.get_or_create(
+                    name=code_produit)
+                stock_obj, stock_created = Stock.objects.update_or_create(
+                    code_magasin="MAGREM PSA", code_zone="REMAN PSA", code_produit=prod_obj
                 )
-                for key in ["ref_cal_out", "ref_psa_out", "open_diag", "ref_mat", "ref_comp", "cal_ktag", "status"]:
-                    try:
-                        del row[key]
-                    except KeyError:
-                        pass
-                try:
-                    # Update or Create SpareParts
-                    part_obj, part_created = SparePart.objects.update_or_create(
-                        code_produit=code_produit, defaults={
-                            "code_zone": "REMAN PSA", "code_magasin": "MAGREM PSA"
-                        }
-                    )
-                    if not part_created:
-                        nb_part_update += 1
 
-                    # Update or create StockParts
-                    prod_obj, part_created = ProductCode.objects.get_or_create(
-                        name=code_produit)
-                    stock_obj, stock_created = Stock.objects.update_or_create(
-                        code_magasin="MAGREM PSA", code_zone="REMAN PSA", code_produit=prod_obj
-                    )
+                # Update or Create EcuType
+                type_values = defaults_dict(EcuType, row, "hw_reference")
+                type_values['spare_part'] = part_obj
+                type_obj, type_created = EcuType.objects.update_or_create(
+                    hw_reference=row['hw_reference'], defaults=type_values
+                )
+                if not type_created:
+                    nb_type_update += 1
 
-                    # Update or Create EcuType
-                    type_values['spare_part'] = part_obj
-                    type_obj, type_created = EcuType.objects.update_or_create(
-                        hw_reference=row.pop("hw_reference"), defaults=type_values
-                    )
-                    if not type_created:
-                        nb_type_update += 1
+                # Update or Create EcurefBase
+                base_values = defaults_dict(EcuRefBase, row, "reman_reference")
+                base_values['ecu_type'] = type_obj
+                base_obj, base_created = EcuRefBase.objects.update_or_create(
+                    reman_reference=reman_reference, defaults=base_values
+                )
+                if not base_created:
+                    nb_base_update += 1
 
-                    # Update or Create EcurefBase
-                    base_values['ecu_type'] = type_obj
-                    base_obj, base_created = EcuRefBase.objects.update_or_create(
-                        reman_reference=reman_reference, defaults=base_values
-                    )
-                    if not base_created:
-                        nb_base_update += 1
+                # Update or Create Ecumodel
+                ecu_model_values = defaults_dict(EcuModel, row, "psa_barcode")
+                ecu_model_values['ecu_type'] = type_obj
+                ecu_obj, ecu_created = EcuModel.objects.update_or_create(
+                    psa_barcode=row['psa_barcode'], defaults=ecu_model_values
+                )
+                if not ecu_created:
+                    nb_ecu_update += 1
+            except KeyError as err:
+                print("KeyError: {}".format(err))
+            except DataError as err:
+                print("DataError: {} - {}".format(reman_reference, err))
+            except IntegrityError as err:
+                print("IntegrityError: {} - {}".format(reman_reference, err))
+            except Stock.MultipleObjectsReturned as err:
+                print("MultipleObjectsReturned: {} - {}".format(code_produit, err))
 
-                    # Update or Create Ecumodel
-                    row['ecu_type'] = type_obj
-                    ecu_obj, ecu_created = EcuModel.objects.update_or_create(
-                        psa_barcode=row.pop("psa_barcode"), defaults=row
-                    )
-                    if not ecu_created:
-                        nb_ecu_update += 1
-                except DataError as err:
-                    print("DataError: {} - {}".format(reman_reference, err))
-                except IntegrityError as err:
-                    print("IntegrityError: {} - {}".format(reman_reference, err))
-                except Stock.MultipleObjectsReturned as err:
-                    print("MultipleObjectsReturned: {} - {}".format(code_produit, err))
-
-            nb_base_after, nb_ecu_after = EcuRefBase.objects.count(), EcuModel.objects.count()
-            self.stdout.write(
-                self.style.SUCCESS(
-                    "[ECUREFBASE] Data update completed: CSV_LINES = {} | ADD = {} | UPDATE = {} | TOTAL = {}".format(
-                        extraction.nrows, nb_base_after - nb_base_before, nb_base_update, nb_base_after
-                    )
+        nb_base_after, nb_ecu_after = EcuRefBase.objects.count(), EcuModel.objects.count()
+        self.stdout.write(
+            self.style.SUCCESS(
+                "[ECUREFBASE] Data update completed: CSV_LINES = {} | ADD = {} | UPDATE = {} | TOTAL = {}".format(
+                    len(data), nb_base_after - nb_base_before, nb_base_update, nb_base_after
                 )
             )
-            self.stdout.write(
-                self.style.SUCCESS(
-                    "[ECUMODEL] Data update completed: CSV_LINES = {} | ADD = {} | UPDATE = {} | TOTAL = {}".format(
-                        extraction.nrows, nb_ecu_after - nb_ecu_before, nb_ecu_update, nb_ecu_after
-                    )
+        )
+        self.stdout.write(
+            self.style.SUCCESS(
+                "[ECUMODEL] Data update completed: CSV_LINES = {} | ADD = {} | UPDATE = {} | TOTAL = {}".format(
+                    len(data), nb_ecu_after - nb_ecu_before, nb_ecu_update, nb_ecu_after
                 )
             )
+        )
+
+    def _export(self):
+        filename = 'base_ref_reman_new'
+        path = os.path.join(CSD_ROOT, conf.EXPORT_PATH)
+        header = [
+            'Reference OE', 'REFERENCE REMAN', 'Module Moteur', 'Réf HW', 'FNR', 'CODE BARRE PSA', 'REF FNR',
+            'REF CAL OUT', 'REF à créer ', 'REF_PSA_OUT', 'OPENDIAG', 'REF_MAT', 'REF_COMP', 'CAL_KTAG', 'STATUT'
+        ]
+        queryset = EcuModel.objects.all().order_by('ecu_type__ecu_ref_base__reman_reference')
+        values_list = (
+            'oe_raw_reference', 'ecu_type__ecu_ref_base__reman_reference', 'ecu_type__technical_data',
+            'ecu_type__hw_reference', 'ecu_type__supplier_oe', 'psa_barcode', 'former_oe_reference',
+            'ecu_type__ecu_ref_base__ref_cal_out', 'ecu_type__spare_part__code_produit',
+            'ecu_type__ecu_ref_base__ref_psa_out', 'ecu_type__ecu_ref_base__open_diag',
+            'ecu_type__ecu_ref_base__ref_mat', 'ecu_type__ecu_ref_base__ref_comp',
+            'ecu_type__ecu_ref_base__cal_ktag',
+            'ecu_type__ecu_ref_base__status'
+
+        )
+        ExportExcel(queryset=queryset, filename=filename, header=header, values_list=values_list).file(path, False)
+        self.stdout.write(
+            self.style.SUCCESS(
+                "Export ECU_REF_BASE completed: NB_REF = {} | FILE = {}.csv".format(
+                    queryset.count(), os.path.join(path, filename)
+                )
+            )
+        )
---
+++ utils/django/models.py
@@ -0,0 +1,6 @@
+def defaults_dict(model, row, *args):
+    fields = [f.name for f in model._meta.local_fields if f.name not in args]
+    defaults = dict(
+        (key, value) for key, value in row.items() if key in fields and value != ""
+    )
+    return defaults
