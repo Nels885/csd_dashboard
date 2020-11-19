b94e800bbe3f5331b6d8d4863a3b1b009f0c7eb7
Nels885 <voirin.lionel@gmail.com>
2020-11-19 14:28:42 +0100

    Addition of the Vehicle information view for the PSA app

---
+++ psa/templatetags/corvet_tags.py
@@ -0,0 +1,15 @@
+from django import template
+
+from psa.models import CorvetChoices
+
+register = template.Library()
+
+
+@register.filter(name='get_corvet')
+def get_corvet(value, arg):
+    """Removes all values of arg from the given string"""
+    try:
+        data = CorvetChoices.objects.get(key=value, column=arg)
+        return data.value
+    except CorvetChoices.DoesNotExist:
+        return value
