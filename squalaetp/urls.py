65fa0324f0bb73b43d7791390171c6cf700bfc80
Nels885 <voirin.lionel@gmail.com>
2020-11-22 16:59:05 +0100

    Modification of the url for editing squalaetp data

--- squalaetp/urls.py
+++ squalaetp/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'squalaetp'
 
 urlpatterns = [
+    path('<int:pk>/edit/', views.SqualaetpUpdateView.as_view(), name='edit'),
+    path('<int:file_id>/detail/', views.detail, name='detail'),
     path('xelon/', views.xelon_table, name='xelon'),
     path('xelon/<int:file_id>/edit/', views.xelon_edit, name='xelon_edit'),
     path('stock-parts/', views.stock_table, name='stock_parts'),
-    path('corvet/<int:pk>/edit/', views.SqualaetpUpdateView.as_view(), name='corvet_edit'),
-    path('<int:file_id>/detail/', views.detail, name='detail'),
     path('log/<str:file>/detail/', views.LogFileView.as_view(), name='log_detail'),
     path('ajax/xelon/', views.ajax_xelon, name='ajax_xelon'),
 ]
