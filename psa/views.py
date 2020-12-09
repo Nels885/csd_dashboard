5834e64c2b210c0b4991e2aad62a538e539b09b6
Nels885 <voirin.lionel@gmail.com>
2020-12-09 21:07:44 +0100

    Fix error of Corvet create form for the success_url redirect

--- psa/views.py
+++ psa/views.py
@@ -118,6 +118,11 @@ class CorvetCreateView(PermissionRequiredMixin, BSModalCreateView):
         context['modal_title'] = _('CORVET integration')
         return context
 
+    def form_valid(self, form):
+        if not self.request.is_ajax():
+            form.save()
+        return super(CorvetCreateView, self).form_valid(form)
+
     def get_success_url(self):
         if 'HTTP_REFERER' in self.request.META:
             return self.request.META['HTTP_REFERER']
