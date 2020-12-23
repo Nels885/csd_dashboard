aebff947bba0216b3861fc4d7c50a45ecd0f4799
Nels885 <voirin.lionel@gmail.com>
2020-12-23 11:00:36 +0100

    Integration of Xelon data update in the Squalaetp cmd

--- squalaetp/management/commands/loadsqualaetp.py
+++ squalaetp/management/commands/loadsqualaetp.py
@@ -1,11 +1,16 @@
 from django.core.management.base import BaseCommand
-from django.core.management.color import no_style
+from django.core.exceptions import FieldDoesNotExist
+from django.db.utils import IntegrityError, DataError
 from django.core.management import call_command
 from django.core.exceptions import ObjectDoesNotExist
-from django.db import connection
 
 from squalaetp.models import Xelon
 from psa.models import Corvet
+from utils.conf import XLS_SQUALAETP_FILE, XLS_DELAY_FILES
+from utils.django.models import defaults_dict
+
+from ._excel_squalaetp import ExcelSqualaetp
+from ._excel_delay_analysis import ExcelDelayAnalysis
 
 
 class Command(BaseCommand):
@@ -19,46 +24,38 @@ class Command(BaseCommand):
             help='Specify import Excel file',
         )
         parser.add_argument(
+            '--xelon_update',
+            action='store_true',
+            dest='xelon_update',
+            help='Update Xelon table',
+        )
+        parser.add_argument(
             '--relations',
             action='store_true',
             dest='relations',
             help='add the relationship between the xelon and corvet tables',
         )
-        parser.add_argument(
-            '--del_relations',
-            action='store_true',
-            dest='del_relations',
-            help='delete all relations',
-        )
-        parser.add_argument(
-            '--delete',
-            action='store_true',
-            dest='delete',
-            help='Delete all data in Squalaetp tables',
-        )
 
     def handle(self, *args, **options):
         self.stdout.write("[SQUALAETP] Waiting...")
 
-        if options['relations']:
+        if options['xelon_update']:
+            if options['filename'] is not None:
+                squalaetp = ExcelSqualaetp(options['filename'])
+            else:
+                squalaetp = ExcelSqualaetp(XLS_SQUALAETP_FILE)
+
+            self._squalaetp_file(Xelon, squalaetp)
+            self._delay_files(Xelon)
+
+        elif options['relations']:
             self._foreignkey_relation()
-
-        elif options['delete']:
-            Xelon.objects.all().delete()
-            Corvet.objects.all().delete()
-
-            sequence_sql = connection.ops.sequence_reset_sql(no_style(), [Xelon, Corvet, ])
-            with connection.cursor() as cursor:
-                for sql in sequence_sql:
-                    cursor.execute(sql)
-            for table in ["Xelon", "Corvet", "CorvetBackup"]:
-                self.stdout.write(self.style.WARNING("Suppression des données de la table {} terminée!".format(table)))
 
         else:
             call_command("loadraspeedi")
             call_command("programing")
             call_command("corvet")
-            call_command("xelon", "--fix_update")
+            call_command("loadsqualaetp", "--xelon_update")
             call_command("indicator")
             call_command("importcorvet", "--squalaetp")
             call_command("exportsqualaetp")
@@ -79,6 +76,66 @@ class Command(BaseCommand):
             self.style.SUCCESS(
                 "[SQUALAETP] Relationships update completed: CORVET/XELON = {} | RASPEEDI/CORVET = {}".format(
                     nb_xelon, nb_corvet
+                )
+            )
+        )
+
+    def _squalaetp_file(self, model, excel):
+        self.stdout.write("[XELON] Waiting...")
+        nb_prod_before = model.objects.count()
+        nb_prod_update = 0
+        for row in excel.xelon_table():
+            xelon_number = row.get("numero_de_dossier")
+            defaults = defaults_dict(model, row, "numero_de_dossier")
+            try:
+                obj, created = model.objects.update_or_create(numero_de_dossier=xelon_number, defaults=defaults)
+                if not created:
+                    nb_prod_update += 1
+            except IntegrityError as err:
+                self.stderr.write(self.style.ERROR("IntegrityError: {} -{}".format(xelon_number, err)))
+            except DataError as err:
+                self.stderr.write(self.style.ERROR("DataError: {} - {}".format(xelon_number, err)))
+        nb_prod_after = model.objects.count()
+        self.stdout.write(
+            self.style.SUCCESS(
+                "[XELON] data update completed: EXCEL_LINES = {} | ADD = {} | UPDATE = {} | TOTAL = {}".format(
+                    excel.nrows, nb_prod_after - nb_prod_before, nb_prod_update, nb_prod_after
+                )
+            )
+        )
+
+    def _delay_files(self, model):
+        self.stdout.write("[DELAY] Waiting...")
+        nb_prod_update = 0
+        excel = ExcelDelayAnalysis(XLS_DELAY_FILES)
+        nb_prod_before = model.objects.count()
+        model.objects.exclude(
+            type_de_cloture__in=['Réparé', 'Rebut'], date_retour__isnull=True).update(type_de_cloture='Réparé')
+        model.objects.filter(actions__isnull=True)
+        for row in excel.table():
+            xelon_number = row.get("numero_de_dossier")
+            defaults = defaults_dict(model, row, "numero_de_dossier")
+            try:
+                obj, created = model.objects.update_or_create(numero_de_dossier=xelon_number, defaults=defaults)
+                if not created:
+                    if not obj.nom_technicien:
+                        obj.type_de_cloture = ''
+                        obj.save()
+                    nb_prod_update += 1
+            except IntegrityError as err:
+                self.stderr.write(self.style.ERROR("IntegrityError row {} : {}".format(xelon_number, err)))
+            except DataError as err:
+                self.stderr.write(self.style.ERROR("DataError row {} : {}".format(xelon_number, err)))
+            except FieldDoesNotExist as err:
+                self.stderr.write(self.style.ERROR("FieldDoesNotExist row {} : {}".format(xelon_number, err)))
+            except KeyError as err:
+                self.stderr.write(self.style.ERROR("KeyError row {} : {}".format(xelon_number, err)))
+
+        nb_prod_after = model.objects.count()
+        self.stdout.write(
+            self.style.SUCCESS(
+                "[DELAY] data update completed: EXCEL_LINES = {} | ADD = {} | UPDATE = {} | TOTAL = {}".format(
+                    excel.nrows, nb_prod_after - nb_prod_before, nb_prod_update, nb_prod_after
                 )
             )
         )
