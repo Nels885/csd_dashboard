7fb20864ab3e68c27936decf47d4fdc2498e80f5
Nels885 <voirin.lionel@gmail.com>
2021-01-07 17:20:48 +0100

    Removed the filter by column for the EcuModel table in the config databables

--- reman/static/reman/js/reman-tables.js
+++ reman/static/reman/js/reman-tables.js
@@ -32,26 +32,6 @@ $(document).ready(function () {
             searchable: false,
             orderable: false,
         }],
-        initComplete: function () {
-            this.api().columns([1, 2, 3, 4, 5, 6, 7, 8, 9]).every(function () {
-                var column = this;
-                var select = $('<select><option value=""></option></select>')
-                    .appendTo($(column.footer()).empty())
-                    .on('change', function () {
-                        var val = $.fn.dataTable.util.escapeRegex(
-                            $(this).val()
-                        );
-
-                        column
-                            .search(val ? '^' + val + '$' : '', true, false)
-                            .draw();
-                    });
-
-                column.data().unique().sort().each(function (d, j) {
-                    select.append('<option value="' + d + '">' + d + '</option>')
-                });
-            });
-        },
     });
 
     $('#outTable').DataTable({
