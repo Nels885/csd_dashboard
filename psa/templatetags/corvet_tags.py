e7627c2066957f558bb3498d2982b43db1fc1fab
Nels885 <voirin.lionel@gmail.com>
2021-01-27 15:06:38 +0100

    Modify get_corvet function of templatetags to parse data

--- psa/templatetags/corvet_tags.py
+++ psa/templatetags/corvet_tags.py
@@ -6,10 +6,17 @@ register = template.Library()
 
 
 @register.filter(name='get_corvet')
-def get_corvet(value, arg):
+def get_corvet(value, args):
     """Removes all values of arg from the given string"""
     try:
-        data = CorvetChoices.objects.get(key=value, column=arg)
-        return data.value
-    except CorvetChoices.DoesNotExist:
+        arg_list = args.split(" ")
+        column = arg_list[0]
+        data = CorvetChoices.objects.get(key=value, column=column).value
+        if len(arg_list) > 1:
+            data_list = data.split("|")
+            select_value = int(arg_list[1])
+            return data_list[select_value]
+        else:
+            return data
+    except (CorvetChoices.DoesNotExist, IndexError):
         return f"* {value} *"
