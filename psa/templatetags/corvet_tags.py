25bdeaaf9f2ceaae4cd8f2b49b4f0d6e7c5484e6
Nels885 <voirin.lionel@gmail.com>
2021-01-28 15:17:11 +0100

    Fix error value in the corvet_tags function

--- psa/templatetags/corvet_tags.py
+++ psa/templatetags/corvet_tags.py
@@ -12,11 +12,13 @@ def get_corvet(value, args):
         arg_list = args.split(" ")
         column = arg_list[0]
         data = CorvetChoices.objects.get(key=value, column=column).value
-        if len(arg_list) > 1:
-            data_list = data.split("|")
+        data_list = data.split("|")
+        if len(arg_list) == 2:
             select_value = int(arg_list[1])
-            return data_list[select_value]
-        else:
-            return data
+            if len(data_list) == select_value + 1:
+                return data_list[select_value]
+            elif arg_list[0] == "DON_MOT" and select_value == 1:
+                return f"* {value} *"
+        return data_list[0]
     except (CorvetChoices.DoesNotExist, IndexError):
         return f"* {value} *"
