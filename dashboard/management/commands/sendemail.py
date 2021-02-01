4a109c617acb756d2a0eae35cbebd7b72983b54e
Nels885 <voirin.lionel@gmail.com>
2021-02-01 15:51:37 +0100

    add cmd for sending email for VINs without CORVET data

--- dashboard/management/commands/sendemail.py
+++ dashboard/management/commands/sendemail.py
@@ -29,9 +29,16 @@ class Command(BaseCommand):
             dest='vin_error',
             help='Send email for VIN error',
         )
+        parser.add_argument(
+            '--vin_corvet',
+            action='store_true',
+            dest='vin_corvet',
+            help='Send email for VIN no CORVET',
+        )
 
     def handle(self, *args, **options):
         date_joined = datetime.strftime(datetime.now(), "%d/%m/%Y %H:%M:%S")
+        last_7_days = timezone.datetime.today() - timezone.timedelta(7)
         if options['late_products']:
             subject = 'Stocks et Retards {}'.format(date_joined)
             prods = ProductAnalysis()
@@ -51,7 +58,6 @@ class Command(BaseCommand):
             )
             self.stdout.write(self.style.SUCCESS("Envoi de l'email des produits en retard terminée!"))
         if options['vin_error']:
-            last_7_days = timezone.datetime.today() - timezone.timedelta(7)
             subject = "Liste d'erreur de VIN Xelon {}".format(date_joined)
             xelons = Xelon.objects.filter(vin_error=True, date_retour__gte=last_7_days).order_by('-date_retour')[:10]
 
@@ -65,3 +71,17 @@ class Command(BaseCommand):
                 self.stdout.write(self.style.SUCCESS("Envoi de l'email des erreurs de VIN terminée !"))
             else:
                 self.stdout.write(self.style.SUCCESS("Pas d'erreurs de VIN a envoyer !"))
+        if options['vin_corvet']:
+            subject = "Liste des VIN Xelon sans CORVET {}".format(date_joined)
+            xelons = Xelon.objects.filter(date_retour__gte=last_7_days, vin__regex=r'^V((F[37])|(R[137]))\w{14}$',
+                                          vin_error=False, corvet__isnull=True).order_by('-date_retour')[:10]
+            if xelons:
+                html_message = render_to_string('dashboard/email_format/vin_corvet_email.html', {'xelons': xelons})
+                plain_message = strip_tags(html_message)
+                send_mail(
+                    subject, plain_message, None, string_to_list(config.VIN_ERROR_TO_EMAIL_LIST),
+                    html_message=html_message
+                )
+                self.stdout.write(self.style.SUCCESS("Envoi de l'email des VINs sans données CORVET terminée !"))
+            else:
+                self.stdout.write(self.style.SUCCESS("Pas de VIN sans données CORVET à envoyer !"))
