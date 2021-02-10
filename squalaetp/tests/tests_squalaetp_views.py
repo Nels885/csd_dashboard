0167f81e121230a3b0a5fd3075f1fa618bc61871
Nels885 <voirin.lionel@gmail.com>
2021-02-10 17:30:39 +0100

    Translation update and adding test for generate view of the Squalaetp app

--- squalaetp/tests/tests_squalaetp_views.py
+++ squalaetp/tests/tests_squalaetp_views.py
@@ -9,18 +9,6 @@ class XelonTestCase(UnitTest):
 
     def setUp(self):
         super(XelonTestCase, self).setUp()
-        self.data = (
-            '<?xml version="1.0" encoding="UTF-8"?><MESSAGE><ENTETE><EMETTEUR>CLARION_PROD</EMETTEUR></ENTETE>'
-            '<VEHICULE Existe="O">'
-            '<DONNEES_VEHICULE><WMI>VF3</WMI><VDS>ABCDEF</VDS><VIS>12345678</VIS>'
-            '<TRANSMISSION>0R</TRANSMISSION></DONNEES_VEHICULE>'
-            '<LISTE_ATTRIBUTS><ATTRIBUT>DAT24</ATTRIBUT><ATTRIBUT>GG805</ATTRIBUT></LISTE_ATTRIBUTS>'
-            '<LISTE_ORGANES><ORGANE>10JBCJ3028478</ORGANE><ORGANE>20DS850837512</ORGANE></LISTE_ORGANES>'
-            '<LISTE_ELECTRONIQUES><ELECTRONIQUE>14A9666571380</ELECTRONIQUE>'
-            '<ELECTRONIQUE>P4A9666220599</ELECTRONIQUE></LISTE_ELECTRONIQUES>'
-            '</VEHICULE></MESSAGE>'
-        )
-        self.vin = 'VF3ABCDEF12345678'
         self.add_perms_user(Xelon, "view_xelon")
         xelon = Xelon.objects.create(numero_de_dossier='A123456789', vin=self.vin, modele_produit='produit',
                                      modele_vehicule='peugeot')
@@ -56,4 +44,10 @@ class XelonTestCase(UnitTest):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-
+    def test_generate_squalaetp_vue(self):
+        url = reverse('squalaetp:generate')
+        response = self.client.get(url)
+        self.assertRedirects(response, self.nextLoginUrl + url, status_code=302)
+        self.login()
+        response = self.client.get(url)
+        self.assertRedirects(response, reverse('index'), status_code=302)
