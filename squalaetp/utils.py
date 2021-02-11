539b893c9d24711b2c319dd5e9293f1e5140482a
Nels885 <voirin.lionel@gmail.com>
2021-02-11 14:01:58 +0100

    Update of the display of multifunction screen data in the product information tab

--- squalaetp/utils.py
+++ squalaetp/utils.py
@@ -4,6 +4,6 @@ def collapse_select(query):
     elif "CALC MOT" in query.famille_produit:
         return {"cmm": True, "ecu": True}
     elif query.modele_produit in ["DISPLAY (COLOR)", "VTH/HUD"]:
-        return {"media": True, "display": True, "audio": True}
+        return {"media": True, "emf": True, "display": True, "audio": True}
     else:
         return {"media": True, "audio": True}
