8f1dc0bfa020e425475f86be958097c83cc6d5ff
nels885 <voirin.lionel@gmail.com>
2021-03-23 13:08:41 +0100

    Fixed duplicate creation when updating Suptech

--- tools/management/commands/suptech.py
+++ tools/management/commands/suptech.py
@@ -49,21 +49,17 @@ class Command(BaseCommand):
 
             elif options['first']:
                 excel = ExcelSuptech(os.path.join(path, filename + ".xls"))
-                self._update_or_create(Suptech, excel.read())
-                csv_file = CsvSuptech(os.path.join(path, filename + ".csv"))
-                self._update_or_create(Suptech, csv_file.read())
-                self._export(path, filename)
+                self._create(Suptech, excel.read())
             else:
                 if os.path.exists(os.path.join(path, filename + ".csv")):
                     csv_file = CsvSuptech(os.path.join(path, filename + ".csv"))
                     self._create(Suptech, csv_file.read())
-                    # os.remove(os.path.join(path, filename + ".csv"))
                     with open(os.path.join(path, filename + ".csv"), "w") as f:
                         f.write("DATE;QUI;XELON;ITEM;TIME;INFO;RMQ;;;;;\r\n")
                 else:
                     self.stdout.write(self.style.WARNING("The file does not exist"))
                 excel = ExcelSuptech(os.path.join(path, filename + ".xls"))
-                self._update_or_create(Suptech, excel.read())
+                self._update(Suptech, excel.read())
                 self._export(path, filename)
         except FileNotFoundError as err:
             logger.error(f"[SUPTECH_CMD] FileNotFoundError: {err}")
@@ -92,30 +88,32 @@ class Command(BaseCommand):
             )
         )
 
-    def _update_or_create(self, model, data):
+    def _update(self, model, data):
         nb_prod_before = model.objects.count()
         nb_prod_update = 0
+        queryset = model.objects.exclude(modified_at__isnull=False)
         for row in data:
             logger.info(row)
-            date = row.pop('date')
-            user = row.pop('user', '')
-            xelon = row.pop('xelon', '')
-            item = row.pop('item', '')
-            time = row.pop('time', '')
-            info = row.pop('info', '')
+            user, xelon = row.get('user', ''), row.get('xelon', '')
+            item, time = row.get('item', ''), row.get('time', '')
+            action = row.pop('action', '')
             try:
-                defaults = defaults_dict(model, row)
-                obj, created = model.objects.update_or_create(
-                    date=date, user=user, xelon=xelon, item=item, time=time, info=info, defaults=defaults
-                )
-                if not created:
-                    nb_prod_update += 1
+                obj = queryset.get(**row)
+                obj.action = action
+                obj.save()
+                nb_prod_update += 1
+            except model.MultipleObjectsReturned as err:
+                self.stdout.write(
+                    self.style.WARNING(f"MultipleObjectsReturned: {user} {xelon} {item} {time} - {err}"))
+            except model.DoesNotExist as err:
+                self.stdout.write(
+                    self.style.WARNING(f"DoesNotExist, modified in Dashboard: {user} {xelon} {item} {time} - {err}"))
             except KeyError as err:
                 self.stderr.write(self.style.ERROR("KeyError: {}".format(err)))
             except IntegrityError as err:
-                self.stderr.write(self.style.ERROR("IntegrityError: {} - {}".format(row.get('item'), err)))
+                self.stderr.write(self.style.ERROR("IntegrityError: {} - {}".format(item, err)))
             except ValidationError as err:
-                self.stderr.write(self.style.ERROR("ValidationError: {} - {}".format(row.get('item'), err)))
+                self.stderr.write(self.style.ERROR("ValidationError: {} - {}".format(item, err)))
         nb_prod_after = model.objects.count()
         self.stdout.write(
             self.style.SUCCESS(
