454d57fa839fcf9514233a8c32267adcee75794a
nels885 <voirin.lionel@gmail.com>
2021-04-14 09:28:35 +0200

    Modification of the EMF foreign key of the Corvet model to associate the new ECU model

--- psa/models.py
+++ psa/models.py
@@ -118,8 +118,8 @@ class Corvet(models.Model):
     radio = models.ForeignKey('Multimedia', related_name='corvet_radio', on_delete=models.SET_NULL, limit_choices_to={'type': 'RAD'}, null=True, blank=True)
     btel = models.ForeignKey('Multimedia', related_name='corvet_btel', on_delete=models.SET_NULL, limit_choices_to={'type': 'NAV'}, null=True, blank=True)
     bsi = models.ForeignKey('psa.Ecu', related_name='corvet_bsi', on_delete=models.SET_NULL, limit_choices_to={'type': 'BSI'}, null=True, blank=True)
-    emf = models.ForeignKey('psa.EmfModel', related_name='corvet_emf', on_delete=models.SET_NULL, null=True, blank=True)
-    # emf = models.ForeignKey('psa.Ecu', related_name='corvet_emf', on_delete=models.SET_NULL, limit_choices_to={'type': 'EMF'}, null=True, blank=True)
+    # emf = models.ForeignKey('psa.EmfModel', related_name='corvet_emf', on_delete=models.SET_NULL, null=True, blank=True)
+    emf = models.ForeignKey('psa.Ecu', related_name='corvet_emf', on_delete=models.SET_NULL, limit_choices_to={'type': 'EMF'}, null=True, blank=True)
     cmm = models.ForeignKey('psa.Ecu', related_name='corvet_cmm', on_delete=models.SET_NULL, limit_choices_to={'type': 'CMM'}, null=True, blank=True)
     bsm = models.ForeignKey('psa.Ecu', related_name='corvet_bsm', on_delete=models.SET_NULL, limit_choices_to={'type': 'BSM'}, null=True, blank=True)
     # hdc = models.ForeignKey('psa.Ecu', related_name='corvet_hdc', on_delete=models.SET_NULL, limit_choices_to={'type': 'HDC'}, null=True, blank=True)
@@ -135,8 +135,8 @@ class Corvet(models.Model):
             self.radio = Multimedia.objects.filter(hw_reference=self.electronique_14f).first()
         if self.electronique_14b.isdigit():
             self.bsi = Ecu.objects.filter(comp_ref__startswith=self.electronique_14b, type='BSI').first()
-        # if self.electronique_14l.isdigit():
-        #     self.emf = EmfModel.objects.filter(hw_reference__startswith=self.electronique_14l).first()
+        if self.electronique_14l.isdigit():
+            self.emf = EmfModel.objects.filter(hw_reference__startswith=self.electronique_14l).first()
         if self.electronique_14l.isdigit():
             self.emf = Ecu.objects.filter(comp_ref__startswith=self.electronique_14l, type='EMF').first()
         if self.electronique_14a.isdigit():
@@ -305,8 +305,8 @@ class Ecu(models.Model):
         super(Ecu, self).save(*args, **kwargs)
         if self.type == "BSI":
             Corvet.objects.filter(electronique_14b__startswith=self.comp_ref).update(bsi=self.pk)
-        # if self.type == "EMF":
-        #     Corvet.objects.filter(electronique_14l__startswith=self.comp_ref).update(emf=self.pk)
+        if self.type == "EMF":
+            Corvet.objects.filter(electronique_14l__startswith=self.comp_ref).update(emf=self.pk)
         if self.type == "MDS":
             Corvet.objects.filter(electronique_19h__startswith=self.comp_ref).update(mds=self.pk)
         if self.type == "CMM":
