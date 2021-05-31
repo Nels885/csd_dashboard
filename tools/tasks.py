d0125f409dcf24ead0bd7de581284cc47482ed5a
nels885 <voirin.lionel@gmail.com>
2021-05-31 11:48:31 +0200

    Moved the conversion to a list of emails in the send_email task for the Tools app

--- tools/tasks.py
+++ tools/tasks.py
@@ -2,6 +2,7 @@ from sbadmin import celery_app
 from django.core.management import call_command
 from django.core.mail import EmailMessage
 
+from utils.conf import string_to_list
 
 @celery_app.task
 def cmd_suptech_task(*args):
@@ -19,7 +20,9 @@ def cmd_suptech_task(*args):
 
 @celery_app.task(bind=True)
 def send_email_task(self, subject, body, from_email, to, cc, files=None):
-    email = EmailMessage(subject=subject, body=body, from_email=from_email, to=to, cc=cc)
+    email = EmailMessage(
+        subject=subject, body=body, from_email=from_email, to=string_to_list(to), cc=string_to_list(cc)
+    )
     if files:
         for f in files:
             email.attach(f.name, f.read(), f.content_type)
