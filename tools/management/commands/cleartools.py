45e2bb076e0a996c28fbe82b366afc8017764460
Nels885 <voirin.lionel@gmail.com>
2021-06-15 16:57:35 +0200

    Adding the bga_time option to the cleartools cmd of the Tools app

--- tools/management/commands/cleartools.py
+++ tools/management/commands/cleartools.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django.core.management.color import no_style
 from django.db import connection
 
-from tools.models import TagXelon, ThermalChamber, Suptech
+from tools.models import TagXelon, ThermalChamber, Suptech, BgaTime
 
 
 class Command(BaseCommand):
@@ -26,6 +26,12 @@ class Command(BaseCommand):
             action='store_true',
             dest='suptech',
             help='Delete all data in Suptech table',
+        )
+        parser.add_argument(
+            '--bga_time',
+            action='store_true',
+            dest='bga_time',
+            help='Delete all data in BgaTime table',
         )
 
     def handle(self, *args, **options):
@@ -56,6 +62,15 @@ class Command(BaseCommand):
                 for sql in sequence_sql:
                     cursor.execute(sql)
             tables.append("Suptech")
+
+        if options['bga_time']:
+            BgaTime.objects.all().delete()
+
+            sequence_sql = connection.ops.sequence_reset_sql(no_style(), [BgaTime, ])
+            with connection.cursor() as cursor:
+                for sql in sequence_sql:
+                    cursor.execute(sql)
+            tables.append("BgaTime")
 
         for table in tables:
             self.stdout.write(self.style.SUCCESS(f"Suppression des données de la table {table} terminée!"))
