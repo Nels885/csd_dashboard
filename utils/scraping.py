66cb39b567c86e8cacb4dd30785e665451e44147
nels885 <voirin.lionel@gmail.com>
2021-08-19 12:54:54 +0200

    Fix error Travis-ci

--- utils/scraping.py
+++ utils/scraping.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import WebDriverException, NoSuchElementException
+from selenium.common.exceptions import WebDriverException
 from constance import config
 
 logger = logging.getLogger('command')
