85ce82f35c665651f9c1b380cbaa885277ddc042
Nels885 <voirin.lionel@gmail.com>
2021-11-24 14:16:28 +0100

    Add pre_save_xelon for the relation with the Corvet model

--- squalaetp/signals.py
+++ squalaetp/signals.py
@@ -2,7 +2,7 @@ import re
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
-from squalaetp.models import Sivin
+from .models import Sivin, Xelon
 from psa.models import Corvet
 
 
@@ -13,3 +13,12 @@ def pre_save_sivin(sender, instance, **kwargs):
             instance.corvet = Corvet.objects.get(vin=instance.codif_vin)
     except Corvet.DoesNotExist as err:
         print(f"DoesNotExist: {instance} => {err}")
+
+
+@receiver(pre_save, sender=Xelon)
+def pre_save_xelon(sender, instance, **kwargs):
+    try:
+        if re.match(r'^[VWZ][FLR0]\w{15}$', str(instance.vin)):
+            instance.corvet = Corvet.objects.get(vin=instance.vin)
+    except Corvet.DoesNotExist:
+        instance.corvet = None
