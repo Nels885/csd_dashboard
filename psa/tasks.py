4f018bdfd0100baf28b2eee28806ebe5e73ca8d6
Nels885 <voirin.lionel@gmail.com>
2021-11-24 10:37:13 +0100

    Addition of the celery task to import CORVET data to the database

---
+++ psa/tasks.py
@@ -0,0 +1,36 @@
+import re
+import time
+from sbadmin import celery_app
+
+from utils.scraping import ScrapingCorvet
+from utils.django.models import defaults_dict
+from utils.django.validators import xml_parser
+from .models import Corvet
+
+
+@celery_app.task
+def save_corvet_to_models(vin):
+    msg = "Not VIN PSA"
+    if re.match(r'^[VWZ][FLR0]\w{15}$', str(vin)):
+        scrap = ScrapingCorvet()
+        start_time = time.time()
+        for attempt in range(2):
+            data = xml_parser(scrap.result(vin))
+            row = xml_parser(data)
+            if scrap.ERROR or "ERREUR COMMUNICATION SYSTEME CORVET" in data:
+                delay_time = time.time() - start_time
+                msg = f"{vin} error CORVET in {delay_time}"
+                break
+            elif row and row.get('donnee_date_entree_montage'):
+                defaults = defaults_dict(Corvet, row, "vin")
+                obj, created = Corvet.objects.update_or_create(vin=row.get("vin"), defaults=defaults)
+                obj.prods.update = False
+                obj.prods.save()
+                delay_time = time.time() - start_time
+                msg = f"{vin} updated in {delay_time}"
+                break
+            if attempt:
+                delay_time = time.time() - start_time
+                msg = f"{vin} error VIN in {delay_time}"
+    print(msg)
+    return msg
