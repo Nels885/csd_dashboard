1d6f7d7360891d0d49fd59bb1a44990dba17c7ff
Nels885 <voirin.lionel@gmail.com>
2021-12-14 16:43:39 +0100

    Added CORVET data export for VIN ICARE list

--- import_export/utils.py
+++ import_export/utils.py
@@ -200,6 +200,9 @@ def extract_corvet(product='corvet'):
     elif product == "ng4":
         header, values_list = BTEL_HEADER, BTEL_FIELDS
         queryset = xelons.filter(corvet__attribut_drc="G4")
+    elif product == "icare":
+        header, values_list = PRODS_HEADER, PRODS_FIELDS
+        queryset = xelons.filter(corvet__isnull=False, corvet__opts__filter="ICARE")
     elif product == "all":
         header, values_list = PRODS_HEADER, PRODS_FIELDS
         queryset = xelons.filter(corvet__isnull=False)
