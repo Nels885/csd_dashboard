c36585c5dff2b38c35689b46fb97846d453ef7e8
nels885 <voirin.lionel@gmail.com>
2021-12-23 11:25:33 +0100

    Fix sparepart relationship update and PSA products

--- squalaetp/management/commands/loadsparepart.py
+++ squalaetp/management/commands/loadsparepart.py
@@ -86,5 +86,8 @@ class Command(BaseCommand):
     def _relation_update(self):
         self.stdout.write("[PRODUCTCODE] Waiting...")
         for ecu in Ecu.objects.all():
-            ProductCode.objects.filter(name__contains=ecu.comp_ref).update(ecu=ecu)
+            if str(ecu.comp_ref)[:-2] == "77":
+                ProductCode.objects.filter(name__contains=str(ecu.comp_ref[:-2])).update(ecu=ecu)
+            else:
+                ProductCode.objects.filter(name__contains=ecu.comp_ref).update(ecu=ecu)
         self.stdout.write(self.style.SUCCESS("[PRODUCTCODE] Data update completed!"))
