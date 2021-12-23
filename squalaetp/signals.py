5c9629c8216c2bb17e7df6c2c9739d3dd2ab79d3
nels885 <voirin.lionel@gmail.com>
2021-12-23 14:37:41 +0100

    Fix IntegrityError when updating Xelon model

--- squalaetp/signals.py
+++ squalaetp/signals.py
@@ -2,7 +2,7 @@ import re
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
-from .models import Sivin, Xelon
+from .models import Sivin, Xelon, ProductCategory
 from psa.models import Corvet, Ecu, Multimedia
 
 
@@ -28,14 +28,28 @@ def pre_save_xelon(sender, instance, **kwargs):
                     comp_ref = instance.corvet.electronique_14f
                 if comp_ref.isdigit():
                     Multimedia.objects.get_or_create(
-                        hw_reference=comp_ref, xelon_name=instance.modele_produit, type=product.corvet_type
-                    )
-            elif product and product.corvet_type == "BSI":
-                comp_ref = instance.corvet.electronique_14b
-                if comp_ref.isdigit():
+                        hw_reference=comp_ref,
+                        defaults={'xelon_name': instance.modele_produit, 'type': product.corvet_type})
+            if product.corvet_type in ["EMF", "CMB", "BSI", "CMM", "HDC", "BSM"]:
+                comp_ref = None
+                if product and product.corvet_type == "EMF":
+                    comp_ref = instance.corvet.electronique_14l
+                elif product and product.corvet_type == "CMB":
+                    comp_ref = instance.corvet.electronique_14k
+                elif product and product.corvet_type == "BSI":
+                    comp_ref = instance.corvet.electronique_14b
+                elif product and product.corvet_type == "CMM":
+                    comp_ref = instance.corvet.electronique_14a
+                elif product and product.corvet_type == "HDC":
+                    comp_ref = instance.corvet.electronique_16p
+                elif product and product.corvet_type == "BSM":
+                    comp_ref = instance.corvet.electronique_16b
+                if comp_ref and comp_ref.isdigit():
                     Ecu.objects.get_or_create(
-                        comp_ref=comp_ref, xelon_name=instance.modele_produit, type=product.corvet_type
+                        comp_ref=comp_ref,
+                        defaults={'xelon_name': instance.modele_produit, 'type': product.corvet_type}
                     )
-
+    except ProductCategory.DoesNotExist:
+        instance.product = None
     except Corvet.DoesNotExist:
         instance.corvet = None
