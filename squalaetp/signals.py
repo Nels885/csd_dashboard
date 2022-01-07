70b2de3e94cefb234dae5c0ae69dc9f2728f90ad
Nels885 <voirin.lionel@gmail.com>
2022-01-07 11:33:22 +0100

    Changed the pre_save signal function of the Xelon model for updating the Xelon product name

--- squalaetp/signals.py
+++ squalaetp/signals.py
@@ -4,6 +4,32 @@ from django.dispatch import receiver
 
 from .models import Sivin, Xelon, ProductCategory
 from psa.models import Corvet, Ecu, Multimedia
+from utils.django.validators import comp_ref_isvalid
+
+
+def product_update(instance):
+    corvet, product = instance.corvet, instance.product
+    if corvet and product:
+        ecu_dict = {
+            "NAV": corvet.electronique_14x, "RAD": corvet.electronique_14f,
+            "EMF": corvet.electronique_14l, "CMB": corvet.electronique_14k, "BSI": corvet.electronique_14b,
+            "CMM": corvet.electronique_14a, "HDC": corvet.electronique_16p, "BSM": corvet.electronique_16b
+        }
+        for corvet_type, comp_ref in ecu_dict.items():
+            if product.corvet_type == corvet_type and comp_ref_isvalid(comp_ref):
+                if product.corvet_type in ["NAV", "RAD"]:
+                    obj, created = Multimedia.objects.get_or_create(
+                        hw_reference=comp_ref,
+                        defaults={'xelon_name': instance.modele_produit, 'type': product.corvet_type})
+                else:
+                    obj, created = Ecu.objects.get_or_create(
+                        comp_ref=comp_ref,
+                        defaults={'xelon_name': instance.modele_produit, 'type': product.corvet_type}
+                    )
+                if not created:
+                    obj.xelon_name = instance.modele_produit
+                    obj.save()
+                break
 
 
 @receiver(pre_save, sender=Sivin)
@@ -23,25 +49,6 @@ def pre_save_xelon(sender, instance, **kwargs):
             instance.vin_error = False
         if instance.modele_produit:
             instance.product, created = ProductCategory.objects.get_or_create(product_model=instance.modele_produit)
-        corvet, product = instance.corvet, instance.product
-        if corvet and product:
-            media_dict = {"NAV": corvet.electronique_14x, "RAD": corvet.electronique_14f}
-            ecu_dict = {
-                "EMF": corvet.electronique_14l, "CMB": corvet.electronique_14k, "BSI": corvet.electronique_14b,
-                "CMM": corvet.electronique_14a, "HDC": corvet.electronique_16p, "BSM": corvet.electronique_16b
-            }
-            for corvet_type, comp_ref in media_dict.items():
-                if product.corvet_type == corvet_type and comp_ref and comp_ref.isdigit():
-                    Multimedia.objects.get_or_create(
-                        hw_reference=comp_ref,
-                        defaults={'xelon_name': instance.modele_produit, 'type': product.corvet_type})
-                    break
-            for corvet_type, comp_ref in ecu_dict.items():
-                if product.corvet_type == corvet_type and comp_ref and comp_ref.isdigit():
-                    Ecu.objects.get_or_create(
-                        comp_ref=comp_ref,
-                        defaults={'xelon_name': instance.modele_produit, 'type': product.corvet_type}
-                    )
-                    break
+        product_update(instance)
     except Corvet.DoesNotExist:
         instance.corvet = None
