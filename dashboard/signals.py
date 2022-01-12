53513895485456984a4370ab00e709baef6f27c9
nels885 <voirin.lionel@gmail.com>
2022-01-12 10:15:11 +0100

    Modifying the signal functions of the Dashboard app

--- dashboard/signals.py
+++ dashboard/signals.py
@@ -11,11 +11,11 @@ from .models import UserProfile, ShowCollapse
 @receiver(post_save, sender=User)
 def create_user_profile(sender, created, instance, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
-        ShowCollapse.objects.create(user=instance)
+        UserProfile.objects.get_or_create(user=instance)
+        ShowCollapse.objects.get_or_create(user=instance)
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=None, **kwargs):
     if created:
-        Token.objects.create(user=instance)
+        Token.objects.get_or_create(user=instance)
