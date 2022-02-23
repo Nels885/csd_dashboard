79e202cf925d73fbb8ab66d8728e1c62fde6fb96
nels885 <voirin.lionel@gmail.com>
2022-02-23 08:21:40 +0100

    Modification of the labels of the spare parts for the repair_edit view

--- reman/static/reman/js/reman-custom.js
+++ reman/static/reman/js/reman-custom.js
@@ -8,12 +8,12 @@
                     for (const row of res.data) {
                         partListElement.innerHTML += `
                             <div class="form-group row">
-                                <label for="form-product_code" class="col-sm-2 col-form-label">Code produit :</label>
+                                <label for="form-product_code" class="col-sm-2 col-form-label">Code produit</label>
                                 <div class="col-sm-3">
                                     <input type="text" class="form-control" id="form-product_code" value="${row.product_code}" readonly>
                                 </div>
 
-                                <label for="form-quantity" class="col-sm-2 col-form-label">Quantité :</label>
+                                <label for="form-quantity" class="col-sm-2 col-form-label">Quantité</label>
                                 <div class="col-sm-1">
                                     <input type="text" class="form-control" id="form-quantity" value="${row.quantity}" readonly>
                                 </div>
