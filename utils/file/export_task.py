af14f60d0038c00725e8129ac37a6a21219023c2
Nels885 <voirin.lionel@gmail.com>
2022-03-16 11:59:00 +0100

    Integration of missing methods in the ExportExcelTask class

--- utils/file/export_task.py
+++ utils/file/export_task.py
@@ -1,7 +1,9 @@
 import tempfile
 import datetime
 import csv
+import xlwt
 import os.path
+import shutil
 
 from django.utils import timezone
 from celery_progress.backend import ProgressRecorder
@@ -17,11 +19,11 @@ class ExportExcelTask(BaseTask):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.date = timezone.now()
         self.sheetName = kwargs.get('sheet_name', 'Feuil1')
         self.noValue = kwargs.get('novalue', "#")
-        self.header = self.fields = []
         self.textCols = kwargs.get('text_cols', [])
+        self.header = kwargs.get('header', [])
+        self.date = timezone.now()
 
     @staticmethod
     def copy_and_get_copied_path():
@@ -45,17 +47,22 @@ class ExportExcelTask(BaseTask):
         path = self.copy_and_get_copied_path()
         destination_path = os.path.join(path, f"{filename}.{excel_type}")
         if excel_type == "csv":
-            self._create_csv(destination_path, self.header, values_list)
+            self._create_csv(destination_path, values_list)
+        elif excel_type == "xls":
+            xldoc = xlwt.Workbook(encoding='utf-8-sig')
+            xldoc = self._create_xls(xldoc, values_list)
+            xldoc.save(destination_path)
         else:
             workbook = Workbook()
-            workbook = self._create_workbook(workbook, self.header, values_list)
+            workbook = self._create_xlsx(workbook, values_list)
             workbook.save(filename=destination_path)
         return destination_path
 
-    def _create_workbook(self, workbook: Workbook, header, values_list):
+    def _create_xlsx(self, workbook: Workbook, values_list):
         """ Formatting data in Excel 2010 format """
         progress_recorder = ProgressRecorder(self)
         # Get active worksheet/tab
+        workbook.encoding = 'utf-8-sig'
         ws = workbook.active
         ws.title = self.sheetName
 
@@ -64,7 +71,7 @@ class ExportExcelTask(BaseTask):
         total_record = len(values_list)
 
         # Assign the titles for each cell of the header
-        for col_num, column_title in enumerate(header, 1):
+        for col_num, column_title in enumerate(self.header, 1):
             cell = ws.cell(row=row_num, column=col_num)
             cell.font = Font(bold=True)
             cell.value = column_title
@@ -84,13 +91,40 @@ class ExportExcelTask(BaseTask):
             progress_recorder.set_progress(row_num + 1, total=total_record, description="Inserting record into row")
         return workbook
 
-    def _create_csv(self, filename, header, values_list):
+    def _create_xls(self, xldoc: xlwt.Workbook, values_list):
+        """ Formatting data in Excel format """
+        progress_recorder = ProgressRecorder(self)
+        sheet = xldoc.add_sheet(self.sheetName)
+
+        # Sheet header, first row
+        row_num = 0
+        total_record = len(values_list)
+
+        font_style = xlwt.XFStyle()
+        font_style.font.bold = True
+
+        for col_num in range(len(self.header)):
+            sheet.write(row_num, col_num, self.header[col_num], font_style)
+
+        # Sheet body, remaining rows
+        font_style = xlwt.XFStyle()
+
+        for i, query in enumerate(values_list):
+            query = tuple([self._html_to_string(_) if isinstance(_, str) else _ for _ in query])
+            query = self._query_format(query)
+            row_num += 1
+            for col_num in range(len(query)):
+                sheet.write(row_num, col_num, query[col_num], font_style)
+            progress_recorder.set_progress(row_num + 1, total=total_record, description="Inserting record into row")
+        return xldoc
+
+    def _create_csv(self, filename, values_list):
         """ Formatting data in CSV format """
         progress_recorder = ProgressRecorder(self)
         total_record = len(values_list)
         with open(filename, 'w+', newline='', encoding='utf-8-sig') as f:
             writer = csv.writer(f, delimiter=';', lineterminator=';\r\n')
-            writer.writerow(header)
+            writer.writerow(self.header)
 
             for row_num, query in enumerate(values_list):
                 query = tuple([self._html_to_string(_, r'[;,]') if isinstance(_, str) else _ for _ in query])
@@ -105,6 +139,14 @@ class ExportExcelTask(BaseTask):
         )
         query = tuple([self.noValue if not value else value for value in query])
         return query
+
+    def _file_yesterday(self, path, file):
+        """ Creation of the backup file d-1 """
+        yesterday = datetime.date.today() - datetime.timedelta(days=1)
+        if os.path.isfile(file):
+            file_date = datetime.datetime.fromtimestamp(os.path.getmtime(file)).date()
+            if file_date >= yesterday:
+                shutil.copyfile(file, os.path.join(path, "{}J-1.{}".format(self.filename, self.excelType)))
 
     @staticmethod
     def _html_to_string(value, re_sub=None):
