08de8a7ed7c8f88691c7fb1dbdf03557d41784cc
Nels885 <voirin.lionel@gmail.com>
2022-03-16 09:15:28 +0100

    Fixed REMAN app testing for VOLVO SEM barcodes

--- reman/tests/__init__.py
+++ reman/tests/__init__.py
@@ -23,7 +23,8 @@ class RemanTest(UnitTest):
 
         # VOLVO REMAN
         sem_type = EcuType.objects.create(hw_reference='85023924.P01', hw_type='NAV', technical_data='SEM')
-        sem_base = EcuRefBase.objects.create(reman_reference='85123456', brand='VOLVO', pf_code='PF832706GK')
+        sem_base = EcuRefBase.objects.create(
+            reman_reference='85123456', brand='VOLVO', pf_code='PF832706GK', ecu_type=sem_type)
         EcuModel.objects.create(barcode='PF832200DF', oe_raw_reference='22996488.P02', ecu_type=sem_type)
         self.semBatch = Batch.objects.create(
             year="V", number=1, quantity=2, customer="VOLVO", created_by=self.user, ecu_ref_base=sem_base)
