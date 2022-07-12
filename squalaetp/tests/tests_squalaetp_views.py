8cc193c55579e594e998cedd00d451444abe7114
Nels885 <voirin.lionel@gmail.com>
2022-07-12 15:54:50 +0200

    Improved view test coverage for the Squalaetp application

--- squalaetp/tests/tests_squalaetp_views.py
+++ squalaetp/tests/tests_squalaetp_views.py
@@ -1,8 +1,10 @@
+import json
+
 from django.urls import reverse
 
 from dashboard.tests.base import UnitTest
-
-from squalaetp.models import Xelon
+from squalaetp.models import Xelon, Sivin
+from psa.models import Corvet
 
 
 class XelonTestCase(UnitTest):
@@ -10,9 +12,11 @@ class XelonTestCase(UnitTest):
     def setUp(self):
         super(XelonTestCase, self).setUp()
         self.add_perms_user(Xelon, "view_xelon")
-        xelon = Xelon.objects.create(numero_de_dossier='A123456789', vin=self.vin, modele_produit='produit',
-                                     modele_vehicule='peugeot')
-        self.xelonId = str(xelon.id)
+        Corvet.objects.create(vin=self.vin, donnee_marque_commerciale="OP", electronique_14x="1234567890",
+                              electronique_94x="1234567890")
+        self.xelon = Xelon.objects.create(numero_de_dossier='A123456789', vin=self.vin, modele_produit='produit',
+                                          modele_vehicule='peugeot')
+        Sivin.objects.create(immat_siv="AB123CD", codif_vin=self.vin)
         self.authError = {"detail": "Informations d'authentification non fournies."}
 
     def test_xelon_table_page(self):
@@ -32,7 +36,7 @@ class XelonTestCase(UnitTest):
         self.assertEqual(response.status_code, 200)
 
     def test_squalaetp_detail_page(self):
-        url = reverse('squalaetp:detail', kwargs={'pk': self.xelonId})
+        url = reverse('squalaetp:detail', kwargs={'pk': self.xelon.id})
         response = self.client.get(url)
         self.assertRedirects(response, self.nextLoginUrl + url, status_code=302)
 
@@ -42,6 +46,11 @@ class XelonTestCase(UnitTest):
         self.assertEqual(response.status_code, 404)
 
         # Detail is valid
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_barcode_pdf_generate(self):
+        url = reverse('squalaetp:barcode_pdf', kwargs={'pk': self.xelon.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
@@ -70,16 +79,30 @@ class XelonTestCase(UnitTest):
         self.assertEqual(response.status_code, 200)
 
     def test_prog_activate_view(self):
-        url = reverse('squalaetp:prog_activate', kwargs={'pk': self.xelonId})
+        url = reverse('squalaetp:prog_activate', kwargs={'pk': self.xelon.id})
         response = self.client.get(url)
-        xelon = Xelon.objects.get(pk=self.xelonId)
+        xelon = Xelon.objects.get(pk=self.xelon.id)
         self.assertRedirects(response, self.nextLoginUrl + url, status_code=302)
         self.assertEqual(xelon.is_active, False)
         self.login()
         response = self.client.get(url)
-        xelon = Xelon.objects.get(pk=self.xelonId)
+        xelon = Xelon.objects.get(pk=self.xelon.id)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(xelon.is_active, True)
+
+    def test_excel_import_async(self):
+        # Excel import is not valid permission
+        url = reverse('squalaetp:excel_import_async')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+        # Excel import is valid permission
+        self.add_perms_user(Xelon, 'add_xelon')
+        self.login()
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content)
+        self.assertEqual(len(data), 1)
 
     def test_xelon_view_set_is_disconnected(self):
         response = self.client.get(reverse('squalaetp:api_xelon-list'), format='json')
@@ -97,5 +120,34 @@ class XelonTestCase(UnitTest):
         response = self.client.get(url)
         self.assertRedirects(response, self.nextLoginUrl + url, status_code=302)
         self.login()
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_sivin_detail_page(self):
+        sivin = Sivin.objects.first()
+        url = reverse('squalaetp:sivin_detail', kwargs={'immat': sivin.immat_siv})
+        response = self.client.get(url)
+        self.assertRedirects(response, self.nextLoginUrl + url, status_code=302)
+
+        # Detail is not found
+        self.login()
+        response = self.client.get(reverse('squalaetp:sivin_detail', kwargs={'immat': 666}))
+        self.assertEqual(response.status_code, 404)
+
+        # Detail is valid
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_log_file_detail_view(self):
+        url = reverse('squalaetp:log_detail', kwargs={'pk': self.xelon.id})
+        response = self.client.get(url)
+        self.assertRedirects(response, self.nextLoginUrl + url, status_code=302)
+
+        # Detail is not found
+        self.login()
+        response = self.client.get(reverse('squalaetp:log_detail', kwargs={'pk': 666}))
+        self.assertEqual(response.status_code, 404)
+
+        # Detail is valid
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
