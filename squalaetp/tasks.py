bc5938d566e84b5420834775544502193d08bbcc
Nels885 <voirin.lionel@gmail.com>
2022-08-04 14:29:16 +0200

    Added tests for tasks from the Squalaetp app

--- squalaetp/tasks.py
+++ squalaetp/tasks.py
@@ -31,25 +31,27 @@ def cmd_importcorvet_task(*args):
     print("cmd_importcorvet_tash in progress...")
     print(f"cmd : importcorvet {' '.join(args)}")
     out = StringIO()
-    call_command("importcorvet", f"{' '.join(args)}", stdout=out)
+    call_command("importcorvet", *args, stdout=out)
     return out.getvalue()
 
 
 @celery_app.task(bind=True)
-def send_email_task(self, subject, body, from_email, to, cc, files=None):
+def send_email_task(self, subject: str, body: str, from_email: str, to: list, cc: list, files=None):
     email = EmailMessage(subject=subject, body=body, from_email=from_email, to=to, cc=cc)
     if files:
         for f in files:
             email.attach(f.name, f.read(), f.content_type)
     email.send()
+    return {"msg": "Envoi email terminé.", "subject": subject, "from_email": from_email, "to": to}
 
 
 @celery_app.task
-def save_sivin_to_models(immat):
+def save_sivin_to_models(immat, **kwargs):
+    test = kwargs.get("test", False)
     msg = "SIVIN not Found"
     if not immat.isnumeric() and (6 < len(immat) < 11):
         start_time = time.time()
-        sivin = ScrapingSivin()
+        sivin = ScrapingSivin(test=test)
         data = xml_sivin_parser(sivin.result(immat))
         sivin.close()
         if sivin.ERROR or "ERREUR COMMUNICATION SYSTEME SIVIN" in data:
---
+++ squalaetp/tests/tests_squalaetp_tasks.py
@@ -0,0 +1,32 @@
+from dashboard.tests.base import UnitTest
+from squalaetp.tasks import (
+    cmd_loadsqualaetp_task, cmd_exportsqualaetp_task, cmd_importcorvet_task, send_email_task, save_sivin_to_models
+)
+
+
+class XelonTaskTestCase(UnitTest):
+
+    def setUp(self):
+        super(XelonTaskTestCase, self).setUp()
+
+    def test_cmd_loadsqualaetp_task(self):
+        response = cmd_loadsqualaetp_task()
+        self.assertDictEqual(response, {"msg": "Importation Squalaetp terminée."})
+
+    def test_cmd_exportsqualaetp_task(self):
+        response = cmd_exportsqualaetp_task()
+        self.assertDictEqual(response, {"msg": "Exportation Squalaetp terminée."})
+
+    def test_cmd_importcorvet_task(self):
+        response = cmd_importcorvet_task("--test", self.vin)
+        self.assertIn("Corvet login Error !!!", response)
+
+    def test_save_sivin_to_models(self):
+        response = save_sivin_to_models("ABCDEFG", test=True)
+        self.assertIn("ABCDEFG - error SIVIN in", response)
+
+    def test_send_email_task(self):
+        response = send_email_task("test", "test", "test@test.com", ["test@test.com"], [])
+        self.assertDictEqual(response, {
+            "msg": "Envoi email terminé.", "subject": "test", "from_email": "test@test.com", "to": ["test@test.com"]
+        })
