4e6bdeb6a0ffda45848cd6402cd9389bf5621373
nels885 <voirin.lionel@gmail.com>
2022-09-13 11:07:26 +0200

    Added Partslink24 scraping class and fixed close() method

--- utils/scraping.py
+++ utils/scraping.py
@@ -10,10 +10,38 @@ from constance import config
 logger = logging.getLogger('command')
 
 
-class ScrapingCorvet(webdriver.Chrome):
-    """ Scraping data Corvet of the repairnav web site"""
-    START_URLS = 'https://www.repairnav.com/clarionservice_v2/corvet.xhtml'
+class Scraping(webdriver.Chrome):
     ERROR = False
+
+    def __init__(self):
+        """ Initialization """
+        options = Options()
+        if config.PROXY_HOST_SCRAPING and config.PROXY_PORT_SCRAPING:
+            options.add_argument(f'proxy-server={config.PROXY_HOST_SCRAPING}:{config.PROXY_PORT_SCRAPING}')
+        options.add_argument('headless')
+        options.add_argument("no-sandbox")  # bypass OS security model
+        options.add_argument("disable-dev-shm-usage")  # overcome limited resource problems
+        super().__init__(executable_path="/usr/local/bin/chromedriver", chrome_options=options)
+        self.set_page_load_timeout(30)
+
+    def close(self, **kwargs):
+        try:
+            if not self.ERROR:
+                self.quit()
+        except Exception as err:
+            self._logger_error('close()', err)
+        finally:
+            self.ERROR = kwargs.get('error', self.ERROR)
+
+    @staticmethod
+    def _logger_error(message, err):
+        exception_type = type(err).__name__
+        logger.error(f"{exception_type} - {message}: {err}")
+
+
+class ScrapingCorvet(Scraping):
+    """ Scraping data Corvet of the repairnav website"""
+    START_URLS = 'https://www.repairnav.com/clarionservice_v2/corvet.xhtml'
 
     def __init__(self, *args, **kwargs):
         """ Initialization """
@@ -21,14 +49,7 @@ class ScrapingCorvet(webdriver.Chrome):
         self.password = kwargs.get('password', config.CORVET_PWD)
         if not kwargs.get('test', False) and self.username and self.password:
             try:
-                options = Options()
-                if config.PROXY_HOST_SCRAPING and config.PROXY_PORT_SCRAPING:
-                    options.add_argument(f'proxy-server={config.PROXY_HOST_SCRAPING}:{config.PROXY_PORT_SCRAPING}')
-                options.add_argument('headless')
-                options.add_argument("no-sandbox")  # bypass OS security model
-                options.add_argument("disable-dev-shm-usage")  # overcome limited resource problems
-                super().__init__(executable_path="/usr/local/bin/chromedriver", chrome_options=options)
-                self.set_page_load_timeout(30)
+                super(ScrapingCorvet, self).__init__()
                 self.get(self.START_URLS)
             except Exception as err:
                 self._logger_error('__init__()', err)
@@ -67,7 +88,7 @@ class ScrapingCorvet(webdriver.Chrome):
 
     def login(self):
         """
-        Login on the web site
+        Login on the website
         """
         try:
             WebDriverWait(self, 10).until(EC.presence_of_element_located((By.NAME, 'form:identifiant2')))
@@ -86,7 +107,7 @@ class ScrapingCorvet(webdriver.Chrome):
 
     def logout(self):
         """
-        Logout on the web site
+        Logout on the website
         :return:
         """
         try:
@@ -97,22 +118,9 @@ class ScrapingCorvet(webdriver.Chrome):
             return False
         return True
 
-    def close(self, **kwargs):
-        self.ERROR = kwargs.get('error', self.ERROR)
-        try:
-            if not self.ERROR:
-                self.quit()
-        except Exception as err:
-            self._logger_error('close()', err)
-
-    @staticmethod
-    def _logger_error(message, err):
-        exception_type = type(err).__name__
-        logger.error(f"{exception_type} - {message}: {err}")
-
 
 class ScrapingSivin(ScrapingCorvet):
-    """ Scraping data SIVIN of the repairnav web site"""
+    """ Scraping data SIVIN of the repairnav website"""
     SIVIN_URLS = 'https://www.repairnav.com/clarionservice_v2/sivin.xhtml'
 
     def result(self, immat_value=None):
@@ -145,3 +153,94 @@ class ScrapingSivin(ScrapingCorvet):
         else:
             data = "Corvet login Error !!!"
         return data
+
+
+class ScrapingPartslink24(Scraping):
+    """ Scraping data Corvet of the partslink24 website"""
+    START_URLS = 'https://www.partslink24.com'
+
+    def __init__(self, *args, **kwargs):
+        """ Initialization """
+        self.account = kwargs.get('account', '')
+        self.user = kwargs.get('user', '')
+        self.password = kwargs.get('password', '')
+        if not kwargs.get('test', False) and self.account and self.user and self.password:
+            try:
+                super(ScrapingPartslink24, self).__init__()
+
+                self.get(self.START_URLS)
+                self.privaty_settings()
+            except Exception as err:
+                self._logger_error('__init__()', err)
+                self.close(error=True)
+        else:
+            self.ERROR = True
+
+    def result(self, vin_value=None):
+        """
+        Corvet data recovery
+        :param vin_value: VIN number for the Corvet data
+        :return: Corvet data
+        """
+        if not self.ERROR and self.login():
+            try:
+                WebDriverWait(self, 10).until(EC.presence_of_element_located((By.NAME, 'form:input_vin'))).clear()
+                vin = self.find_element_by_name('form:input_vin')
+                submit = self.find_element_by_id('form:suite')
+                if vin_value:
+                    vin.send_keys(vin_value)
+                submit.click()
+                time.sleep(1)
+                data = WebDriverWait(self, 10).until(
+                    EC.presence_of_element_located((By.NAME, 'form:resultat_CORVET'))
+                ).text
+                if data and len(data) == 0:
+                    data = "ERREUR COMMUNICATION SYSTEME CORVET"
+            except Exception as err:
+                self._logger_error('CORVET result()', err)
+                data = "Exception or timeout error !"
+                self.ERROR = True
+            self.logout()
+        else:
+            data = "Corvet login Error !!!"
+        return data
+
+    def privaty_settings(self):
+        WebDriverWait(self, 10).until(EC.presence_of_element_located((By.XPATH, '//button[@data-testid="uc-accept-all-button"]')))
+        accept_all = self.find_element_by_xpath('//button[@data-testid="uc-accept-all-button"]')
+        accept_all.click()
+
+    def login(self):
+        """
+        Login on the website
+        """
+        try:
+            WebDriverWait(self, 10).until(EC.presence_of_element_located((By.NAME, 'accountLogin')))
+            account = self.find_element_by_name('accountLogin')
+            user = self.find_element_by_name('userLogin')
+            password = self.find_element_by_name('loginBean.password')
+            for element, value in {account: self.account, user: self.user, password: self.password}.items():
+                element.clear()
+                element.send_keys(value)
+            login = self.find_element_by_id('login-btn')
+            login.click()
+            if self.find_element_by_id('squeezeout-login-btn'):
+                self.find_element_by_id('squeezeout-login-btn').click()
+        except Exception as err:
+            self._logger_error('login()', err)
+            self.close(error=True)
+            return False
+        return True
+
+    def logout(self):
+        """
+        Logout on the website
+        :return:
+        """
+        try:
+            WebDriverWait(self, 10).until(EC.presence_of_element_located((By.ID, 'logoutLink'))).click()
+        except Exception as err:
+            self._logger_error('logout()', err)
+            self.quit()
+            return False
+        return True
