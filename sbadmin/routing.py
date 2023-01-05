2071fbf9040e187d6edb52716f03d0d6ecb2d444
nels885 <voirin.lionel@gmail.com>
2023-01-05 08:57:47 +0100

    Fix routing protocol

--- sbadmin/routing.py
+++ sbadmin/routing.py
@@ -1,5 +1,9 @@
 from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
+    "http": django_asgi_app,
 })
