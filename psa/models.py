0f70db233c49f7bed1ca409ca33cf913e19298e0
nels885 <voirin.lionel@gmail.com>
2023-02-15 12:48:09 +0100

    Addition of VMF modules from the manufacturer PSA in the product detail view

--- psa/models.py
+++ psa/models.py
@@ -278,6 +278,7 @@ class CorvetProduct(models.Model):
     fmux = models.ForeignKey('psa.Ecu', related_name='corvet_fmux', on_delete=models.SET_NULL, limit_choices_to={'type': 'FMUX'}, null=True, blank=True)
     mds = models.ForeignKey('psa.Ecu', related_name='corvet_mds', on_delete=models.SET_NULL, limit_choices_to={'type': 'MDS'}, null=True, blank=True)
     cvm2 = models.ForeignKey('psa.Ecu', related_name='corvet_cvm2', on_delete=models.SET_NULL, limit_choices_to={'type': 'CVM2'}, null=True, blank=True)
+    vmf = models.ForeignKey('psa.Ecu', related_name='corvet_vmf', on_delete=models.SET_NULL, limit_choices_to={'type': 'VMF'}, null=True, blank=True)
 
     class Meta:
         verbose_name = "produits CORVET"
@@ -429,7 +430,7 @@ class Ecu(models.Model):
         ('CMB', 'Combine Planche de Bord'), ('CMM', 'Calculateur Moteur Multifonction'),
         ('EMF', 'Ecran Multifonctions'), ('FMUX', 'Façade Multiplexée'),
         ('HDC', 'Haut de Colonne de Direction (COM200x)'), ('MDS', 'Module de service telematique'),
-        ('CVM2', 'Camera Video Multifonction V2')
+        ('CVM2', 'Camera Video Multifonction V2'), ('VMF', 'Module Commutation Integre')
     ]
 
     comp_ref = models.CharField("réf. comp. matériel", max_length=10, unique=True)
