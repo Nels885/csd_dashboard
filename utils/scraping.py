284de99b41a9f5d97db1d32b1af6f321104a7082
nels885 <voirin.lionel@gmail.com>
2023-02-16 15:00:38 +0100

    Fix error scraping with chromedriver_autoinstaller

--- utils/scraping.py
+++ utils/scraping.py
@@ -1,10 +1,10 @@
 import time
 import logging
 import xml.etree.ElementTree as ET
-import chromedriver_autoinstaller
 from datetime import datetime
 from django.utils.timezone import make_aware
 
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.by import By
@@ -16,8 +16,6 @@ from constance import config
 
 
 logger = logging.getLogger('command')
-
-chromedriver_autoinstaller.install()
 
 
 class Scraping(webdriver.Chrome):
@@ -33,7 +31,7 @@ class Scraping(webdriver.Chrome):
             options.add_argument('headless')
         options.add_argument("no-sandbox")  # bypass OS security model
         options.add_argument("disable-dev-shm-usage")  # overcome limited resource problems
-        super().__init__(chrome_options=options)
+        super().__init__(ChromeDriverManager().install(), chrome_options=options)
         self.set_page_load_timeout(30)
         self.STATUS = "INIT"
 
