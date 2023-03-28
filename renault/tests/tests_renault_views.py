8930ca1762efd4e1310c4f5125a0e0da070293cf
nels885 <voirin.lionel@gmail.com>
2023-03-28 21:47:34 +0200

    Added missing tests for Renault app

--- renault/tests/tests_renault_views.py
+++ renault/tests/tests_renault_views.py
@@ -11,3 +11,13 @@ class RenaultTestCase(UnitTest):
     def test_useful_links_page(self):
         response = self.client.get(reverse('renault:useful_links'))
         self.assertEqual(response.status_code, 200)
+
+    def test_tool_page(self):
+        response = self.client.get(reverse('renault:tools'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_ajax_decode(self):
+        response = self.client.get(reverse('renault:ajax_decode'))
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(
+            response.content, {'result': 'ERROR', 'message': 'Could not compute the code with a empty precode !'})
