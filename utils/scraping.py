7a643500ea265afb70e3689fa0e6d94420e4e5a7
nels885 <voirin.lionel@gmail.com>
2023-04-05 11:38:20 +0200

    Changing module for installing chromedriver

--- utils/scraping.py
+++ utils/scraping.py
@@ -4,7 +4,8 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 from django.utils.timezone import make_aware
 
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
+import chromedriver_autoinstaller
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -42,7 +43,8 @@ class Scraping(webdriver.Chrome):
         if kwargs.get('headless', True):
             options.add_argument('headless')
         # super().__init__(ChromeDriverManager().install(), options=options, seleniumwire_options=options_seleniumWire)
-        super().__init__(service=ChromeService(ChromeDriverManager().install()), options=options)
+        # super().__init__(service=ChromeService(ChromeDriverManager().install()), options=options)
+        super().__init__(service=ChromeService(chromedriver_autoinstaller.install()), options=options)
         self.set_page_load_timeout(30)
 
     def is_element_exist(self, by, value):
