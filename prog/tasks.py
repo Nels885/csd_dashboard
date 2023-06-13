f19ac10189bc2267f33fada043ca346d065e3fcb
Thomas Garette <thomas.garette@forvia.com>
2023-06-13 11:46:14 +0200

    Added 120s timeout to send_firmware_task

--- prog/tasks.py
+++ prog/tasks.py
@@ -1,3 +1,5 @@
+import requests
+import time
 from websockets.sync.client import connect
 from sbadmin import celery_app
 from django.core.files.storage import default_storage
@@ -5,8 +7,9 @@ from django.core.files.storage import default_storage
 from prog.models import MbedFirmware
 
 
-@celery_app.task
-def send_firmware_task(ws_uri, fw_name, target):
+@celery_app.task(bind=True)
+def send_firmware_task(self, raspi_url, fw_name, target):
+    ws_uri = "ws://" + raspi_url + ":8080/updateMbed"
     try:
         selected_firmware = MbedFirmware.objects.get(name=fw_name)
         with connect(str(ws_uri)) as wsocket:
@@ -21,7 +24,19 @@ def send_firmware_task(ws_uri, fw_name, target):
                         f.close()
                         break
                     wsocket.send(file_data)
-        msg = {"msg": "Succès : Envoi du firmware avec succès !"}
+        time.sleep(1)
+        timeout = 0
+        while True:
+            response = requests.get(url="http://" + raspi_url + "/api/info/", timeout=(0.05, 0.5))
+            progress = response.json()['percent']
+            timeout += 1
+            if progress == '100' :
+                msg = {"msg": "Succès : Mise à jour du mbed avec succès !"}
+                break
+            if timeout > 60:
+                msg = {"msg": "Erreur : Timeout!", "tags": "warning"}
+                break
+            time.sleep(2)
     except MbedFirmware.DoesNotExist:
         msg = {"msg": "Not found", "tags": "warning"}
     return msg
