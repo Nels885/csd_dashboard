b94cb8933a91d7fe8a1d4a0c561bae4eb2eedc87
nels885 <voirin.lionel@gmail.com>
2023-11-21 11:40:38 +0100

    Adding the boolean templatetag

--- dashboard/templatetags/custom_tags.py
+++ dashboard/templatetags/custom_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.translation import gettext as _
 
 register = template.Library()
 
@@ -6,3 +7,12 @@ register = template.Library()
 @register.filter(name='range')
 def filter_range(start, end):
     return range(start, end)
+
+
+@register.filter(name='boolean')
+def boolean(value):
+    if isinstance(value, bool) and value is True:
+        return _('Yes')
+    if value.isdigit() and int(value) == 1:
+        return _('Yes')
+    return _('No')
