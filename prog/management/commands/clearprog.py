368963b5737fe7c0a8932c4ca5d18b812cc2c384
nels885 <voirin.lionel@gmail.com>
2024-09-25 11:14:01 +0200

    Modified deamonAET command and added clear commands for new models

--- prog/management/commands/clearprog.py
+++ prog/management/commands/clearprog.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django.core.management.color import no_style
 from django.db import connection
 
-from prog.models import Raspeedi, Programing
+from prog.models import Raspeedi, Programing, AETLog, AETMeasure
 
 
 class Command(BaseCommand):
@@ -20,6 +20,18 @@ class Command(BaseCommand):
             action='store_true',
             dest='programing',
             help='Clear Programing table',
+        )
+        parser.add_argument(
+            '--aetlog',
+            action='store_true',
+            dest='aetlog',
+            help='Clear AETLog table',
+        )
+        parser.add_argument(
+            '--aetmeasure',
+            action='store_true',
+            dest='aetmeasure',
+            help='Clear AETMeasure table',
         )
 
     def handle(self, *args, **options):
@@ -40,3 +52,19 @@ class Command(BaseCommand):
                 for sql in sequence_sql:
                     cursor.execute(sql)
             self.stdout.write(self.style.WARNING(message.format('Programing')))
+        if options['aetlog']:
+            AETLog.objects.all().delete()
+
+            sequence_sql = connection.ops.sequence_reset_sql(no_style(), [AETLog, ])
+            with connection.cursor() as cursor:
+                for sql in sequence_sql:
+                    cursor.execute(sql)
+            self.stdout.write(self.style.WARNING(message.format('AETLog')))
+        if options['aetmeasure']:
+            AETMeasure.objects.all().delete()
+
+            sequence_sql = connection.ops.sequence_reset_sql(no_style(), [AETMeasure, ])
+            with connection.cursor() as cursor:
+                for sql in sequence_sql:
+                    cursor.execute(sql)
+            self.stdout.write(self.style.WARNING(message.format('AETMeasure')))
--- prog/management/commands/daemonAET.py
+++ prog/management/commands/daemonAET.py
@@ -6,7 +6,11 @@ from dateutil.relativedelta import relativedelta
 from django.utils import timezone
 from django.utils.timezone import make_aware
 from django.core.management.base import BaseCommand
-from constance import config
+
+from utils.conf import CSD_ROOT
+from utils.django.models import defaults_dict
+
+from prog.models import AETLog, AETMeasure
 
 
 class Command(BaseCommand):
@@ -29,28 +33,28 @@ class Command(BaseCommand):
         prod_ref = ["DCM3.5", "DCM6.2A", "DCM6.2C", "E98", "EDC15C2", "EDC16C34", "EDC17C60", "EDC17C84", "ME17.9.52", "VD46.1"]
         new_files = []
         for ref in prod_ref:
-            path = os.path.join(config.CSD_DIR, f'LOGS/LOG_AET/{ref}/*/*{last_24_hours}*.csv')
-            new_files = new_files + glob(path, recursive=True)
+            path = os.path.join(CSD_ROOT, f'LOGS/LOG_AET/{ref}/*/*{last_24_hours}*.csv')
+            for file in glob(path, recursive=True):
+                new_files.append((file, ref))
 
         all_products_list_dictt = []
         for new_file, ref in new_files:
             with open(new_file, "r") as file:
                 rows = file.read().splitlines()
                 data2 = []
-                # print("vin", vin_from_filename(new_file))
                 if not re.search(r'[a-zA-Z]123456789', new_file):
                     for row in rows:
                         data2.append(row.split(";"))
                     if self.aet_check(data2):
                         list_dictt = self.data2dict(data2, new_file, ref)
                         all_products_list_dictt.append(list_dictt)
-        print(all_products_list_dictt)
+        self._import_data(all_products_list_dictt)
 
     @staticmethod
     def get_value(name, value):
         try:
             if isinstance(name, str):
-                values = ['XELON', 'MAT_REF', 'COMP_REF']
+                values = ['XELON', 'MANU_REF', 'COMP_REF']
                 data = name.split("_")
                 index = values.index(value)
                 if len(data) >= index:
@@ -92,20 +96,39 @@ class Command(BaseCommand):
         for line in data:
             try:
                 if '' not in line:
-                    # print(line)
-                    dictt["REF"] = ref
-                    dictt["AET"] = data[0][2]
-                    dictt["XELON"] = self.get_value(name, "XELON")
-                    dictt["MAT_REF"] = self.get_value(name, 'MAT_REF')
-                    dictt["COMP_REF"] = self.get_value(name, 'COMP_REF')
-                    dictt["DATE"] = make_aware(datetime.fromtimestamp(mtime))
-                    dictt["MEASURE_NAME"] = line[1]
-                    dictt["VALUE"] = line[2].replace(",", ".")
-                    dictt["LOWER_BOND"] = line[4].replace(",", ".")
-                    dictt["UPPER_BOND"] = line[5].replace(",", ".")
-                    # print(dictt)
+                    dictt["prod_name"] = ref
+                    dictt["aet_name"] = ""
+                    if "38-" in data[0][2]:
+                        dictt["aet_name"] = data[0][2]
+                    dictt["xelon"] = self.get_value(name, "XELON")
+                    dictt["manu_ref"] = self.get_value(name, 'MANU_REF')
+                    dictt["comp_ref"] = self.get_value(name, 'COMP_REF')
+                    dictt["date"] = make_aware(datetime.fromtimestamp(mtime))
+                    dictt["measure_name"] = line[1]
+                    dictt["measured_value"] = line[2].replace(",", ".")
+                    dictt["min_value"] = line[4].replace(",", ".")
+                    dictt["max_value"] = line[5].replace(",", ".")
                     list_dictt.append(dictt.copy())
             except Exception:
                 pass
-        # print(list_dictt)
         return list_dictt
+
+    def _import_data(self, data):
+        nb_before = AETLog.objects.count()
+        nb_update = 0
+        for file in data:
+            for row in file:
+                log_keys = ['xelon', 'comp_ref', 'manu_ref', 'prod_name', 'aet_name', 'date']
+                log_values = defaults_dict(AETLog, {key: row.pop(key, '') for key in log_keys})
+                measure_values = defaults_dict(AETMeasure, row)
+                obj, created = AETLog.objects.get_or_create(**log_values)
+                if not created:
+                    nb_update += 1
+                obj.measures.get_or_create(**measure_values)
+        nb_after = AETLog.objects.count()
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"[DAEMON_AET_CMD] data update completed: ADD = {nb_after - nb_before} | UPDATE = {nb_update} | " +
+                f"TOTAL = {nb_after}"
+            )
+        )
